70dc338a4726c8ef1570e4b85d67167ce65ac3e9
Dave Watkins <davidwatkins73@gmail.com>
2016-04-10 18:46:59 +0100

    Improving performance of emp view
         #128

--- waltz-jobs/src/main/java/com/khartec/waltz/jobs/sample/InvolvementGenerator.java
+++ waltz-jobs/src/main/java/com/khartec/waltz/jobs/sample/InvolvementGenerator.java
@@ -17,21 +17,28 @@
 
 package com.khartec.waltz.jobs.sample;
 
-import com.khartec.waltz.common.MapBuilder;
 import com.khartec.waltz.model.EntityKind;
 import com.khartec.waltz.model.EntityReference;
 import com.khartec.waltz.model.ImmutableEntityReference;
-import com.khartec.waltz.model.involvement.ImmutableInvolvement;
-import com.khartec.waltz.model.involvement.Involvement;
+import com.khartec.waltz.model.application.ApplicationKind;
 import com.khartec.waltz.model.involvement.InvolvementKind;
+import com.khartec.waltz.schema.tables.records.InvolvementRecord;
+import com.khartec.waltz.service.DIConfiguration;
+import org.jooq.DSLContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static com.khartec.waltz.common.ListUtilities.concat;
 import static com.khartec.waltz.common.ListUtilities.randomPick;
+import static com.khartec.waltz.schema.tables.Application.APPLICATION;
+import static com.khartec.waltz.schema.tables.Involvement.INVOLVEMENT;
+import static com.khartec.waltz.schema.tables.OrganisationalUnit.ORGANISATIONAL_UNIT;
+import static com.khartec.waltz.schema.tables.Person.PERSON;
 
 
 public class InvolvementGenerator {
@@ -42,115 +49,107 @@ public class InvolvementGenerator {
 
     public static void main(String[] args) {
 
-//        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(DIConfiguration.class);
-//
-//        DataSource ds = ctx.getBean(DataSource.class);
-//        NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(ds);
-//
-//        List<String> developers = template.queryForList("SELECT employee_id FROM person WHERE title LIKE '%developer%'", new HashMap<>(), String.class);
-//        List<String> managers = template.queryForList("SELECT employee_id FROM person WHERE title LIKE '%manager%'", new HashMap<>(), String.class);
-//        List<String> analysts = template.queryForList("SELECT employee_id FROM person WHERE title LIKE '%analyst%'", new HashMap<>(), String.class);
-//        List<String> administrators = template.queryForList("SELECT employee_id FROM person WHERE title LIKE '%administrator%'", new HashMap<>(), String.class);
-//        List<String> qa = template.queryForList("SELECT employee_id FROM person WHERE title LIKE '%qa%'", new HashMap<>(), String.class);
-//
-//        List<String> directors = template.queryForList("SELECT employee_id FROM person WHERE title LIKE '%director%' AND title NOT LIKE '%sales%'", new HashMap<>(), String.class);
-//
-//        List<Long> orgUnitIds = template.queryForList("SELECT id FROM organisational_unit", new HashMap<>(), Long.class);
-//
-//
-//        List<Long> inHouseApps = template.queryForList("SELECT id FROM application WHERE kind = 'IN_HOUSE'", new HashMap<>(), Long.class);
-//        List<Long> hostedApps = template.queryForList("SELECT id FROM application WHERE kind = 'INTERNALLY_HOSTED'", new HashMap<>(), Long.class);
-//        List<Long> externalApps = template.queryForList("SELECT id FROM application WHERE kind = 'EXTERNALLY_HOSTED'", new HashMap<>(), Long.class);
-//        List<Long> eucApps = template.queryForList("SELECT id FROM application WHERE kind = 'EUC'", new HashMap<>(), Long.class);
-//
-//        List<Involvement> devInvolvements = inHouseApps.stream()
-//                .map(id -> toAppRef(id))
-//                .flatMap(appRef -> mkInvolvments(appRef, developers, InvolvementKind.DEVELOPER, 7))
-//                .collect(Collectors.toList());
-//
-//        List<Involvement> qaInvolvements = concat(inHouseApps, hostedApps)
-//                .stream()
-//                .map(id -> toAppRef(id))
-//                .flatMap(appRef -> mkInvolvments(appRef, qa, InvolvementKind.QA, 3))
-//                .collect(Collectors.toList());
-//
-//        List<Involvement> projectManagerInvolvements = concat(inHouseApps, externalApps, hostedApps, eucApps)
-//                .stream()
-//                .map(id -> toAppRef(id))
-//                .flatMap(appRef -> mkInvolvments(appRef, managers, InvolvementKind.PROJECT_MANAGER, 1))
-//                .collect(Collectors.toList());
-//
-//        List<Involvement> supportManagerInvolvments = concat(inHouseApps, externalApps, hostedApps)
-//                .stream()
-//                .map(id -> toAppRef(id))
-//                .flatMap(appRef -> mkInvolvments(appRef, managers, InvolvementKind.SUPPORT_MANAGER, 1))
-//                .collect(Collectors.toList());
-//
-//        List<Involvement> analystInvolvments = concat(inHouseApps, externalApps, hostedApps)
-//                .stream()
-//                .map(id -> toAppRef(id))
-//                .flatMap(appRef -> mkInvolvments(appRef, analysts, InvolvementKind.BUSINESS_ANALYST, 3))
-//                .collect(Collectors.toList());
-//
-//        List<Involvement> ouArchitects = orgUnitIds.stream()
-//                .map(id -> ImmutableInvolvement.builder()
-//                        .employeeId(randomPick(directors))
-//                        .entityReference(ImmutableEntityReference.builder().kind(EntityKind.ORG_UNIT).id(id).build())
-//                        .kind(InvolvementKind.IT_ARCHITECT)
-//                        .build())
-//                .collect(Collectors.toList());
-//
-//        List<Involvement> ouSponsors = orgUnitIds.stream()
-//                .map(id -> ImmutableInvolvement.builder()
-//                        .employeeId(randomPick(directors))
-//                        .entityReference(ImmutableEntityReference.builder().kind(EntityKind.ORG_UNIT).id(id).build())
-//                        .kind(InvolvementKind.BUSINESS_SPONSOR)
-//                        .build())
-//                .collect(Collectors.toList());
-//
-//        template.update("DELETE FROM involvement", new HashMap<>());
-//
-//        String insertSql = "INSERT INTO involvement(entity_kind, entity_id, kind, employee_id) " +
-//                " VALUES (:entityKind, :entityId, :kind, :employeeId)";
-//
-//        template.batchUpdate(insertSql, toParams(devInvolvements));
-//        template.batchUpdate(insertSql, toParams(qaInvolvements));
-//        template.batchUpdate(insertSql, toParams(supportManagerInvolvments));
-//        template.batchUpdate(insertSql, toParams(projectManagerInvolvements));
-//        template.batchUpdate(insertSql, toParams(analystInvolvments));
-//        template.batchUpdate(insertSql, toParams(ouArchitects));
-//        template.batchUpdate(insertSql, toParams(ouSponsors));
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(DIConfiguration.class);
+
+        DSLContext dsl = ctx.getBean(DSLContext.class);
+
+        List<String> developers = getEmployeeIdsByTitle(dsl, "%developer%");
+        List<String> managers = getEmployeeIdsByTitle(dsl, "%manager%");
+        List<String> analysts = getEmployeeIdsByTitle(dsl, "%analyst%");
+        List<String> administrators = getEmployeeIdsByTitle(dsl, "%administrator%");
+        List<String> qa = getEmployeeIdsByTitle(dsl, "%qa%");
+        List<String> directors = getEmployeeIdsByTitle(dsl, "%director%");;
+
+        List<Long> orgUnitIds = dsl.select(ORGANISATIONAL_UNIT.ID)
+                .from(ORGANISATIONAL_UNIT)
+                .fetch(ORGANISATIONAL_UNIT.ID);
+
+        List<Long> inHouseApps = getAppIdsByKind(dsl, ApplicationKind.IN_HOUSE);
+        List<Long> hostedApps = getAppIdsByKind(dsl, ApplicationKind.INTERNALLY_HOSTED);
+        List<Long> externalApps = getAppIdsByKind(dsl, ApplicationKind.EXTERNALLY_HOSTED);
+        List<Long> eucApps = getAppIdsByKind(dsl, ApplicationKind.EUC);
+
+        List<InvolvementRecord> devInvolvements = inHouseApps.stream()
+                .map(id -> toAppRef(id))
+                .flatMap(appRef -> mkInvolvments(appRef, developers, InvolvementKind.DEVELOPER, 7))
+                .collect(Collectors.toList());
+
+        List<InvolvementRecord> qaInvolvements = concat(inHouseApps, hostedApps)
+                .stream()
+                .map(id -> toAppRef(id))
+                .flatMap(appRef -> mkInvolvments(appRef, qa, InvolvementKind.QA, 3))
+                .collect(Collectors.toList());
+
+        List<InvolvementRecord> projectManagerInvolvements = concat(inHouseApps, externalApps, hostedApps, eucApps)
+                .stream()
+                .map(id -> toAppRef(id))
+                .flatMap(appRef -> mkInvolvments(appRef, managers, InvolvementKind.PROJECT_MANAGER, 1))
+                .collect(Collectors.toList());
+
+        List<InvolvementRecord> supportManagerInvolvments = concat(inHouseApps, externalApps, hostedApps)
+                .stream()
+                .map(id -> toAppRef(id))
+                .flatMap(appRef -> mkInvolvments(appRef, managers, InvolvementKind.SUPPORT_MANAGER, 1))
+                .collect(Collectors.toList());
+
+        List<InvolvementRecord> analystInvolvments = concat(inHouseApps, externalApps, hostedApps)
+                .stream()
+                .map(id -> toAppRef(id))
+                .flatMap(appRef -> mkInvolvments(appRef, analysts, InvolvementKind.BUSINESS_ANALYST, 3))
+                .collect(Collectors.toList());
+
+        List<InvolvementRecord> ouArchitects = orgUnitIds.stream()
+                .map(id -> new InvolvementRecord(
+                        EntityKind.ORG_UNIT.name(),
+                        id,
+                        InvolvementKind.IT_ARCHITECT.name(),
+                        randomPick(directors),
+                        "RANDOM_GENERATOR"))
+                .collect(Collectors.toList());
+
+        List<InvolvementRecord> ouSponsors = orgUnitIds.stream()
+                .map(id -> new InvolvementRecord(
+                        EntityKind.ORG_UNIT.name(),
+                        id,
+                        InvolvementKind.BUSINESS_SPONSOR.name(),
+                        randomPick(directors),
+                        "RANDOM_GENERATOR"))
+                .collect(Collectors.toList());
+
+        dsl.delete(INVOLVEMENT).execute();
+
+        dsl.batchInsert(devInvolvements).execute();
+        dsl.batchInsert(qaInvolvements).execute();
+        dsl.batchInsert(supportManagerInvolvments).execute();
+        dsl.batchInsert(projectManagerInvolvements).execute();
+        dsl.batchInsert(analystInvolvments).execute();
+        dsl.batchInsert(ouArchitects).execute();
+        dsl.batchInsert(ouSponsors).execute();
+
 
         System.out.println("Done");
 
     }
 
-    private static Stream<Involvement> mkInvolvments(EntityReference appRef, List<String> employeeIds, InvolvementKind kind, int upperBound) {
+    private static List<Long> getAppIdsByKind(DSLContext dsl, ApplicationKind kind) {
+        return dsl.select(APPLICATION.ID).from(APPLICATION).where(APPLICATION.KIND.eq(kind.name())).fetch(APPLICATION.ID);
+    }
+
+    private static List<String> getEmployeeIdsByTitle(DSLContext dsl, String title) {
+        return dsl.select(PERSON.EMPLOYEE_ID).from(PERSON).where(PERSON.TITLE.like(title)).fetch(PERSON.EMPLOYEE_ID);
+    }
+
+    private static Stream<InvolvementRecord> mkInvolvments(EntityReference appRef, List<String> employeeIds, InvolvementKind kind, int upperBound) {
         int count = rnd.nextInt(upperBound) + 1;
-        List<Involvement> involvements = new ArrayList<>(count);
-
-        for (int i = 0; i < count; i++) {
-            Involvement involvement = ImmutableInvolvement.builder()
-                    .kind(kind)
-                    .employeeId(randomPick(employeeIds))
-                    .entityReference(appRef)
-                    .build();
-            involvements.add(involvement);
-        }
-        return involvements.stream();
+        return IntStream.range(0, count)
+                .mapToObj(i -> new InvolvementRecord(
+                        appRef.kind().name(),
+                        appRef.id(),
+                        kind.name(),
+                        randomPick(employeeIds),
+                        "RANDOM_GENERATOR"));
     }
 
-
-    private static Map[] toParams(List<Involvement> involvements) {
-        return  involvements.stream()
-                .map(i -> new MapBuilder()
-                        .add("entityKind", i.entityReference().kind().name())
-                        .add("entityId", i.entityReference().id())
-                        .add("kind", i.kind().name())
-                        .add("employeeId", i.employeeId())
-                        .build())
-                .toArray(Map[]::new);
-    }
 
 
     private static EntityReference toAppRef(Long id) {
