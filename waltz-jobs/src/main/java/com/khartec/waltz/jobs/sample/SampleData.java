5db1b521ea4778842aa77ce9bdff815345d1ce69
Kamran <kamran.saleem@hotmail.co.uk>
2016-06-21 12:33:22 +0100

    - fix Maven shade plugin not downloaded - specify version - sample data for server country and os version - sample liquibase migrate .bat for mssql
    #226

--- waltz-jobs/src/main/java/com/khartec/waltz/jobs/sample/SampleData.java
+++ waltz-jobs/src/main/java/com/khartec/waltz/jobs/sample/SampleData.java
@@ -96,4 +96,16 @@ public interface SampleData {
             "AIX",
             "AS/400"
     };
+
+
+    String[] operatingSystemVersions = new String[] {
+            "1.0",
+            "1.1",
+            "2.0",
+            "3.0",
+            "2000",
+            "XP",
+            "ME",
+            "Vista"
+    };
 }
