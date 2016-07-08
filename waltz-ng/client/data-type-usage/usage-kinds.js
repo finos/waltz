e0e7fe734c43188b64d5423878eb1ed21b05094f
Dave Watkins <davidwatkins73@gmail.com>
2016-07-08 11:11:24 +0100

    Tidy up Auth sources
    #266

--- waltz-ng/client/data-type-usage/usage-kinds.js
+++ waltz-ng/client/data-type-usage/usage-kinds.js
@@ -2,21 +2,25 @@ const usageKinds = [
     {
         displayName: 'Consumer',
         kind: "CONSUMER",
+        nameDescription: "A consumer is an application that acquires data from an upstream source",
         readOnly: true,
         readOnlyDescription: 'This value is derived from the presence of <strong>incoming</strong> flows'
     }, {
         displayName: 'Distributor',
         kind: "DISTRIBUTOR",
+        nameDescription: "A distributor is an application that provides data to one or more downstream applications",
         readOnly: true,
         readOnlyDescription: 'This value is derived from the presence of <strong>outgoing</strong> flows'
     }, {
         displayName: 'Modifier',
         kind: "MODIFIER",
+        nameDescription: "A modifier is an application that changes the data acquired from an upstream source. Changes may be as a result of an application function or direct manipulation in a GUI",
         readOnly: false,
         readOnlyDescription: ''
     }, {
         displayName: 'Originator / Manual Entry',
         kind: "ORIGINATOR",
+        nameDescription: "An originator is an application that captures data via a GUI or other input type (e.g. scanner, smartphone, card reader)",
         readOnly: false,
         readOnlyDescription: ''
     }
