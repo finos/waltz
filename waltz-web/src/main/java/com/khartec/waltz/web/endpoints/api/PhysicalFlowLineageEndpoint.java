40beab9ea4373782405cbee4d26f4f5f9dcef9c6
Kamran <kamran@thinkincode.co.uk>
2016-10-21 17:16:48 +0100

    Lineage: add change log entries
    #738

--- waltz-web/src/main/java/com/khartec/waltz/web/endpoints/api/PhysicalFlowLineageEndpoint.java
+++ waltz-web/src/main/java/com/khartec/waltz/web/endpoints/api/PhysicalFlowLineageEndpoint.java
@@ -1,9 +1,15 @@
 package com.khartec.waltz.web.endpoints.api;
 
+import com.khartec.waltz.model.EntityKind;
+import com.khartec.waltz.model.EntityReference;
 import com.khartec.waltz.model.LastUpdate;
+import com.khartec.waltz.model.Severity;
+import com.khartec.waltz.model.changelog.ChangeLog;
+import com.khartec.waltz.model.changelog.ImmutableChangeLog;
 import com.khartec.waltz.model.command.CommandResponse;
 import com.khartec.waltz.model.physical_flow_lineage.*;
 import com.khartec.waltz.model.user.Role;
+import com.khartec.waltz.service.changelog.ChangeLogService;
 import com.khartec.waltz.service.physical_flow_lineage.PhysicalFlowLineageService;
 import com.khartec.waltz.service.user.UserRoleService;
 import com.khartec.waltz.web.ListRoute;
@@ -25,14 +31,20 @@ public class PhysicalFlowLineageEndpoint implements Endpoint {
     private static final String BASE_URL = mkPath("api", "physical-flow-lineage");
 
 
+    private final ChangeLogService changeLogService;
     private final PhysicalFlowLineageService physicalFlowLineageService;
     private final UserRoleService userRoleService;
 
 
     @Autowired
-    public PhysicalFlowLineageEndpoint(PhysicalFlowLineageService physicalFlowLineageService, UserRoleService userRoleService) {
+    public PhysicalFlowLineageEndpoint(ChangeLogService changeLogService,
+                                       PhysicalFlowLineageService physicalFlowLineageService,
+                                       UserRoleService userRoleService) {
+        checkNotNull(changeLogService, "changeLogService cannot be null");
         checkNotNull(physicalFlowLineageService, "physicalFlowLineageService cannot be null");
         checkNotNull(userRoleService, "userRoleService cannot be null");
+
+        this.changeLogService = changeLogService;
         this.physicalFlowLineageService = physicalFlowLineageService;
         this.userRoleService = userRoleService;
     }
@@ -79,6 +91,7 @@ public class PhysicalFlowLineageEndpoint implements Endpoint {
         putForDatum(addContributionPath, this::addContribution);
     }
 
+
     private CommandResponse<PhysicalFlowLineageRemoveCommand> removeContribution(Request request, Response response) {
         requireRole(userRoleService, request, Role.LINEAGE_EDITOR);
 
@@ -86,6 +99,10 @@ public class PhysicalFlowLineageEndpoint implements Endpoint {
                 .describedFlowId(getLong(request,"describedFlowId"))
                 .contributingFlowId(getLong(request, "contributorFlowId"))
                 .build();
+
+        logChange(getUsername(request),
+                EntityReference.mkRef(EntityKind.PHYSICAL_FLOW, removeCommand.describedFlowId()),
+                String.format("Removing contribution of flow: %s from lineage", removeCommand.contributingFlowId()));
 
         return physicalFlowLineageService.removeContribution(removeCommand);
     }
@@ -96,12 +113,32 @@ public class PhysicalFlowLineageEndpoint implements Endpoint {
 
         Long contributingFlowId = readBody(request, Long.class);
 
+        String username = getUsername(request);
+
         PhysicalFlowLineageAddCommand addCommand = ImmutablePhysicalFlowLineageAddCommand.builder()
                 .describedFlowId(getLong(request,"describedFlowId"))
                 .contributingFlowId(contributingFlowId)
-                .lastUpdate(LastUpdate.mkForUser(getUsername(request)))
+                .lastUpdate(LastUpdate.mkForUser(username))
                 .build();
 
+        logChange(username,
+                EntityReference.mkRef(EntityKind.PHYSICAL_FLOW, addCommand.describedFlowId()),
+                String.format("Adding contribution of flow: %s to lineage", addCommand.contributingFlowId()));
+
         return physicalFlowLineageService.addContribution(addCommand);
+    }
+
+
+    private void logChange(String userId,
+                           EntityReference ref,
+                           String message) {
+        ChangeLog logEntry = ImmutableChangeLog.builder()
+                .parentReference(ref)
+                .message(message)
+                .severity(Severity.INFORMATION)
+                .userId(userId)
+                .build();
+
+        changeLogService.write(logEntry);
     }
 }
