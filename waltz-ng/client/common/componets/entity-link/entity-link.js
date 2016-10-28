f51cdbec9701bd5d53be4de96de5668afd82e784
Rohit Vats <rohit.vats@gmail.com>
2016-10-28 13:33:44 +0100

    Remove viewState binding
    #856

--- waltz-ng/client/common/componets/entity-link/entity-link.js
+++ waltz-ng/client/common/componets/entity-link/entity-link.js
@@ -22,11 +22,11 @@ function controller($state) {
 
     vm.$onChanges = (changes) => {
         if (vm.entityRef) {
-            vm.viewState = kindToViewState(vm.entityRef.kind);
-            if (vm.viewState) {
+            const viewState = kindToViewState(vm.entityRef.kind);
+            if (viewState) {
                 // url needs to be re-computed when entityRef changes
                 // eg: when used in a ui-grid cell template
-                vm.viewUrl = $state.href(vm.viewState, { id: vm.entityRef.id });
+                vm.viewUrl = $state.href(viewState, { id: vm.entityRef.id });
             }
         }
     };
