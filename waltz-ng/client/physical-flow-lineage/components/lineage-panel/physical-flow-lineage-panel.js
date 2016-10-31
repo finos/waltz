5b9da426e2653f4e8a05244241e10eeba3b1c0e6
Rohit Vats <rohit.vats@gmail.com>
2016-10-31 15:33:37 +0000

    Lineage reports: Allow export from physical flow screen
    #835

--- waltz-ng/client/physical-flow-lineage/components/lineage-panel/physical-flow-lineage-panel.js
+++ waltz-ng/client/physical-flow-lineage/components/lineage-panel/physical-flow-lineage-panel.js
@@ -1,15 +1,57 @@
+import {initialiseData, mkEntityLinkGridCell, mkLinkGridCell} from "../../../common";
+
 
 const template = require('./physical-flow-lineage-panel.html');
 
 
 const bindings = {
-    lineage: '<'
+    lineage: '<',
+    onInitialise: '<'
 };
+
+
+const initialState = {
+    lineage: [],
+    onInitialise: (e) => {}
+};
+
+
+function controller() {
+
+    const vm = initialiseData(this, initialState);
+
+    vm.columnDefs = [
+        Object.assign(mkLinkGridCell('Specification', 'specification.name', 'flow.id', 'main.physical-flow.view'), { width: "20%"} ),
+        Object.assign(mkEntityLinkGridCell('From', 'sourceEntity', 'left'), { width: "15%" }),
+        Object.assign(mkEntityLinkGridCell('To', 'targetEntity', 'left'), { width: "15%" }),
+        { field: 'specification.format', displayName: 'Format', width: "12%" },
+        { field: 'flow.transport', displayName: 'Transport', width: "12%" },
+        { field: 'flow.description', displayName: 'Description', width: "26%" }
+    ];
+
+    vm.onGridInitialise = (e) => {
+        vm.exportFn = e.exportFn;
+    };
+
+    vm.exportGrid = () => {
+        vm.exportFn('lineage.csv');
+    };
+
+    // callback
+    vm.onInitialise({
+        exportFn: vm.exportGrid
+    });
+
+}
+
+
+controller.$inject = [];
 
 
 const component = {
     bindings,
-    template
+    template,
+    controller
 };
 
 
--- waltz-ng/client/physical-specifications/components/mentions/physical-specification-mentions.js
+++ waltz-ng/client/physical-specifications/components/mentions/physical-specification-mentions.js
@@ -1,13 +1,54 @@
+import {initialiseData, mkEntityLinkGridCell, mkLinkGridCell} from "../../../common";
+
 const bindings = {
-    mentions: '<'
+    mentions: '<',
+    onInitialise: '<'
 };
+
+
+const initialState = {
+    mentions: [],
+    onInitialise: (e) => {}
+};
+
 
 const template = require('./physical-specification-mentions.html');
 
 
+function controller() {
+
+    const vm = initialiseData(this, initialState);
+
+    vm.columnDefs = [
+        Object.assign(mkLinkGridCell('Specification', 'specification.name', 'flow.id', 'main.physical-flow.view'), { width: "20%"} ),
+        Object.assign(mkEntityLinkGridCell('From', 'sourceEntity', 'left'), { width: "13%" }),
+        Object.assign(mkEntityLinkGridCell('To', 'targetEntity', 'left'), { width: "13%" }),
+        { field: 'specification.format', displayName: 'Format', width: "10%" },
+        { field: 'flow.transport', displayName: 'Transport', width: "12%" },
+        { field: 'flow.frequency', displayName: 'Frequency', width: "10%" },
+        { field: 'specification.description', displayName: 'Description', width: "22%" }
+    ];
+
+    vm.onGridInitialise = (e) => {
+        vm.exportFn = e.exportFn;
+    };
+
+    vm.exportGrid = () => {
+        vm.exportFn('mentions.csv');
+    };
+
+    // callback
+    vm.onInitialise({
+        exportFn: vm.exportGrid
+    });
+
+}
+
+
 const component = {
     template,
-    bindings
+    bindings,
+    controller
 };
 
 
