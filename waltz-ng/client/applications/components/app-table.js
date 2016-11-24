0267d6420aa8131cbd38354b75d67c909bfdf39c
Dave Watkins <davidwatkins73@gmail.com>
2016-11-24 08:23:56 +0000

    Normalising apps-section across:
    - app group
    - capability view
    - unit view
    - person view
    
    #1058

--- waltz-ng/client/applications/components/app-table.js
+++ waltz-ng/client/applications/components/app-table.js
@@ -10,101 +10,74 @@
  *
  */
 
-import _ from "lodash";
-import {
-    lifecyclePhaseDisplayNames,
-    applicationKindDisplayNames,
-    investmentRatingNames
-} from "../../common/services/display_names";
-
 
 const bindings = {
     applications: '<',
-    csvName: '@?'
+    onInitialise: '<'
 };
 
 
-function controller(uiGridConstants, $animate) {
+const columnDefs = [
+    {
+        width: 40,
+        field: 'info',
+        displayName: '',
+        cellTemplate: `<div class="ui-grid-cell-contents text-center"> 
+                         <a class="clickable" 
+                            ng-click="grid.appScope.$ctrl.onAppSelect(row.entity)"> 
+                            <waltz-icon name="info-circle" 
+                                        size="lg"></waltz-icon>
+                         </a>
+                       </div>`
+    }, {
+        field: 'name',
+        cellTemplate: `<div class="ui-grid-cell-contents" 
+                            ng-switch="row.entity['management']">
+                         <span ng-switch-when="End User" 
+                               ng-bind="COL_FIELD"></span>
+                         <a ng-switch-default
+                            ui-sref="main.app.view ({ id: row.entity['id'] })" 
+                            ng-bind="COL_FIELD">
+                         </a>
+                       </div>`
+    }, {
+        field: 'assetCode'
+    },{
+        field: 'name',
+        cellTemplate: `<div class="ui-grid-cell-contents">
+                          <span ng-bind="row.entity['management'] "></span>
+                          <span ng-bind="COL_FIELD"></span>
+                       </div>`
+    }, {
+        field: 'kind',
+        cellFilter: "toDisplayName:'applicationKind'",
+    }, {
+        field: 'overallRating',
+        cellFilter: "toDisplayName:'investmentRating'",
+    }, {
+        field: 'riskRating',
+        cellFilter: "toDisplayName:'criticality'",
+    }, {
+        field: 'businessCriticality',
+        cellFilter: "toDisplayName:'criticality'",
+    }, {
+        field: 'lifecyclePhase',
+        cellFilter: "toDisplayName:'lifecyclePhase'",
+    }
+];
+
+
+function controller() {
 
     const vm = this;
 
-    const csvName = angular.isDefined(vm.csvName) ? vm.csvName : 'applications.csv';
-    vm.selectedApp = undefined;
+    vm.columnDefs = columnDefs;
 
-    vm.gridOptions = {
-        enableGridMenu: true,
-        exporterCsvFilename: csvName,
-        exporterMenuPdf: false,
-        enableSorting: true,
-        enableFiltering: true,
-        enableHorizontalScrollbar: uiGridConstants.scrollbars.NEVER,
-        onRegisterApi: (gridApi) => {
-            $animate.enabled(gridApi.grid.element, false);
-        },
-        columnDefs: [
-            {
-                field: 'info',
-                displayName: '',
-                enableSorting: false,
-                enableColumnMenu: false,
-                enableFiltering: false,
-                width: 40,
-                cellTemplate: `<div class="ui-grid-cell-contents text-center"> 
-                                 <a class="clickable" 
-                                     ng-click="grid.appScope.$ctrl.onAppSelect(row.entity)"> 
-                                    <waltz-icon name="info-circle" 
-                                                size="lg"></waltz-icon>
-                                 </a>
-                               </div>`
-            }, {
-                field: 'name',
-                cellTemplate: `<div class="ui-grid-cell-contents" ng-switch="row.entity['management']">
-                                 <span ng-switch-when="row.entity['management'] == 'End User'" 
-                                      ng-bind="COL_FIELD"></span>
-                                 <a ng-switch-default
-                                    ui-sref="main.app.view ({ id: row.entity['id'] })" 
-                                    ng-bind="COL_FIELD">
-                                 </a>
-                               </div>`
-            }, {
-                field: 'assetCode'
-            }, {
-                field: 'kind',
-                cellFilter: "toDisplayName:'applicationKind'",
-                filter: {
-                    type: uiGridConstants.filter.SELECT,
-                    selectOptions: _.map(applicationKindDisplayNames, (label, value) => ({ label, value }))
-                }
-            }, {
-                field: 'overallRating',
-                cellFilter: "toDisplayName:'investmentRating'",
-                filter: {
-                    type: uiGridConstants.filter.SELECT,
-                    selectOptions: _.map(investmentRatingNames, (label, value) => ({ label, value }))
-                }
-            }, {
-                field: 'lifecyclePhase',
-                cellFilter: "toDisplayName:'lifecyclePhase'",
-                filter: {
-                    type: uiGridConstants.filter.SELECT,
-                    selectOptions: _.map(lifecyclePhaseDisplayNames, (label, value) => ({ label, value }))
-                }
-            }
-        ],
-        data: vm.applications
-    };
-
-    vm.onAppSelect = app => {
-        vm.selectedApp = app;
-    };
-
-    vm.$onChanges= () => vm.gridOptions.data = vm.applications || [];
+    vm.$onChanges= () => vm.gridData = vm.applications || [];
 }
 
 
 controller.$inject = [
-    'uiGridConstants',
-    '$animate'
 ];
 
 
