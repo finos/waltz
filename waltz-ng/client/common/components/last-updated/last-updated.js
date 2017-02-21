7f54a79aab40105256eb4b37b04b2d480843bee8
Kamran <kamran@thinkincode.co.uk>
2017-02-21 12:13:21 +0000

    last-updated widget
    #1523

---
+++ waltz-ng/client/common/components/last-updated/last-updated.js
@@ -0,0 +1,16 @@
+const bindings = {
+    lastUpdatedBy: '<',
+    lastUpdatedAt: '<'
+};
+
+
+const template = require('./last-updated.html');
+
+
+const component = {
+    bindings,
+    template,
+};
+
+
+export default component;
