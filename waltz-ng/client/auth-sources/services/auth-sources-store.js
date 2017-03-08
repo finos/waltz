eb9e8cdfe83336eac642aaa7d850a0631b86098e
Rohit Vats <rohit.vats@gmail.com>
2017-03-08 16:10:22 +0000

    Merge branch 'master' into waltz-1735-remove-app-cap-enum
    # Conflicts:
    #	waltz-data/src/main/ddl/liquibase/db.changelog-1.3.xml

--- waltz-ng/client/auth-sources/services/auth-sources-store.js
+++ waltz-ng/client/auth-sources/services/auth-sources-store.js
@@ -1,4 +1,3 @@
-
 /*
  * Waltz - Enterprise Architecture
  * Copyright (C) 2016  Khartec Ltd.
@@ -16,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 import {checkIsIdSelector} from "../../common/checks";
 
 
