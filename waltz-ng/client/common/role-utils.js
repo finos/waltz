0b676889377d1f80dca868d4509c3e3ba1abfbe3
kamransaleem <kamran@thinkincode.co.uk>
2017-05-12 14:55:45 +0100

    waltz-has-role-for-entity-kind directive and apply to person/entity association edit button
    #2167

---
+++ waltz-ng/client/common/role-utils.js
@@ -0,0 +1,40 @@
+/*
+ *
+ *  * Waltz - Enterprise Architecture
+ *  * Copyright (C) 2017  Khartec Ltd.
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU Lesser General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU Lesser General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Lesser General Public License
+ *  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/**
+ * Given an entity kind, returns the user roles required for editing
+ * @param kind
+ * @returns role
+ */
+export function getEditRoleForEntityKind(kind) {
+    switch (kind) {
+        case 'APPLICATION':
+            return 'APP_EDITOR';
+        case 'CHANGE_INITIATIVE':
+            return 'CHANGE_INITIATIVE_EDITOR';
+        case 'MEASURABLE':
+            return 'CAPABILITY_EDITOR';
+        case 'ORG_UNIT':
+            return 'ORG_UNIT_EDITOR';
+        default:
+            return 'ADMIN';
+    }
+}
---
+++ waltz-ng/client/user/directives/has-role-for-entity-kind.js
@@ -0,0 +1,52 @@
+/*
+ *
+ *  * Waltz - Enterprise Architecture
+ *  * Copyright (C) 2017  Khartec Ltd.
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU Lesser General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU Lesser General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Lesser General Public License
+ *  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import {getEditRoleForEntityKind} from '../../common/role-utils';
+
+
+const BINDINGS = {
+    entityKind: '@waltzHasRoleForEntityKind'
+};
+
+
+function controller(UserService) {
+    const vm = this;
+    const role = getEditRoleForEntityKind(vm.entityKind);
+    UserService
+        .whoami()
+        .then(user => vm.show = UserService.hasRole(user, role));
+
+    vm.show = false;
+}
+
+
+controller.$inject = ['UserService'];
+
+
+export default () => ({
+    replace: true,
+    restrict: 'A',
+    transclude: true,
+    scope: {},
+    bindToController: BINDINGS,
+    controllerAs: 'ctrl',
+    template: '<span ng-show="ctrl.show"><ng-transclude></ng-transclude></span>',
+    controller
+});
