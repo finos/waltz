ce126e9e8f02d3b54a9b57bc6bad5b6bb5827c7c
Rohit Vats <rohit.vats@gmail.com>
2017-05-16 11:27:31 +0100

    Playpen 5
    #2187

---
+++ waltz-ng/client/playpen/5/index.js
@@ -0,0 +1,41 @@
+/*
+ * Waltz - Enterprise Architecture
+ * Copyright (C) 2016  Khartec Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import angular from "angular";
+
+import playpenView5 from "./playpen5";
+
+
+export default () => {
+
+    const module = angular.module('waltz.playpen5', []);
+
+    module.config([
+        '$stateProvider',
+        ($stateProvider) => {
+            $stateProvider
+                .state('main.playpen.5', {
+                    url: '/5',
+                    views: { 'content@': playpenView5 }
+                })
+        }
+    ]);
+
+    return module.name;
+
+};
---
+++ waltz-ng/client/playpen/5/playpen5.js
@@ -0,0 +1,44 @@
+/*
+ * Waltz - Enterprise Architecture
+ * Copyright (C) 2016  Khartec Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+const initialState = {
+};
+
+
+function controller() {
+
+    const vm = Object.assign(this, initialState);
+
+}
+
+
+controller.$inject = [
+];
+
+
+const view = {
+    template: require('./playpen5.html'),
+    controller,
+    controllerAs: 'ctrl',
+    bindToController: true,
+    scope: {}
+};
+
+
+export default view;
