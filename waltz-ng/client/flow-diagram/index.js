a446bae6f9ebed0e607693e42f75da290fce5eeb
kamransaleem <kamran@thinkincode.co.uk>
2017-05-24 14:54:26 +0100

    Flow Diagram: allow disjointed nodes
    #2207

--- waltz-ng/client/flow-diagram/index.js
+++ waltz-ng/client/flow-diagram/index.js
@@ -18,29 +18,48 @@
 
 import angular from "angular";
 
+
+import flowDiagramStore from './services/flow-diagram-store';
+import flowDiagramAnnotationStore from './services/flow-diagram-annotation-store';
+import flowDiagramEntityStore from './services/flow-diagram-entity-store';
+import flowDiagramStateService from './services/flow-diagram-state-service';
+
+import flowDiagram from './components/diagram/flow-diagram';
+import flowDiagramLogicalFlowPopup from './components/editor/flow-diagram-logical-flow-popup';
+import flowDiagramNodePopup from './components/editor/flow-diagram-node-popup';
+import flowDiagramPhysicalFlowPopup from './components/editor/flow-diagram-physical-flow-popup';
+import flowDiagramAnnotationPopup from './components/editor/flow-diagram-annotation-popup';
+import flowDiagramEditor from './components/editor/flow-diagram-editor';
+import flowDiagramsPanel from './components/diagrams-panel/flow-diagrams-panel';
+import flowDiagramsSection from './components/section/flow-diagrams-section';
+
+import routes from './routes';
+
+
 export default () => {
 
     const module = angular.module('waltz.flow-diagram', []);
 
     module
-        .service('FlowDiagramStore', require('./services/flow-diagram-store'))
-        .service('FlowDiagramAnnotationStore', require('./services/flow-diagram-annotation-store'))
-        .service('FlowDiagramEntityStore', require('./services/flow-diagram-entity-store'))
-        .service('FlowDiagramStateService', require('./services/flow-diagram-state-service'))
+        .service('FlowDiagramStore', flowDiagramStore)
+        .service('FlowDiagramAnnotationStore', flowDiagramAnnotationStore)
+        .service('FlowDiagramEntityStore', flowDiagramEntityStore)
+        .service('FlowDiagramStateService', flowDiagramStateService)
         ;
 
     module
-        .component('waltzFlowDiagram', require('./components/diagram/flow-diagram'))
-        .component('waltzFlowDiagramLogicalFlowPopup', require('./components/editor/flow-diagram-logical-flow-popup'))
-        .component('waltzFlowDiagramPhysicalFlowPopup', require('./components/editor/flow-diagram-physical-flow-popup'))
-        .component('waltzFlowDiagramAnnotationPopup', require('./components/editor/flow-diagram-annotation-popup'))
-        .component('waltzFlowDiagramEditor', require('./components/editor/flow-diagram-editor'))
-        .component('waltzFlowDiagramsPanel', require('./components/diagrams-panel/flow-diagrams-panel'))
-        .component('waltzFlowDiagramsSection', require('./components/section/flow-diagrams-section'))
+        .component('waltzFlowDiagram', flowDiagram)
+        .component('waltzFlowDiagramLogicalFlowPopup', flowDiagramLogicalFlowPopup)
+        .component('waltzFlowDiagramNodePopup', flowDiagramNodePopup)
+        .component('waltzFlowDiagramPhysicalFlowPopup', flowDiagramPhysicalFlowPopup)
+        .component('waltzFlowDiagramAnnotationPopup', flowDiagramAnnotationPopup)
+        .component('waltzFlowDiagramEditor', flowDiagramEditor)
+        .component('waltzFlowDiagramsPanel', flowDiagramsPanel)
+        .component('waltzFlowDiagramsSection', flowDiagramsSection)
         ;
 
     module
-        .config(require('./routes'))
+        .config(routes)
         ;
 
     return module.name;
