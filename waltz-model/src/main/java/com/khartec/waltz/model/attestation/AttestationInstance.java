69f98c664c3271824b915fb954088a4f9d8885db
salekam <kamran.saleem@db.com>
2017-08-14 17:10:25 +0100

    Attestation: Submission of Attestations
    CTCTOWALTZ-576

---
+++ waltz-model/src/main/java/com/khartec/waltz/model/attestation/AttestationInstance.java
@@ -0,0 +1,29 @@
+package com.khartec.waltz.model.attestation;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.khartec.waltz.model.EntityKind;
+import com.khartec.waltz.model.EntityReference;
+import com.khartec.waltz.model.IdProvider;
+import com.khartec.waltz.model.Nullable;
+import org.immutables.value.Value;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableAttestationInstance.class)
+@JsonDeserialize(as = ImmutableAttestationInstance.class)
+public abstract class AttestationInstance implements IdProvider {
+
+    public abstract Long attestationRunId();
+    public abstract EntityReference parentEntity();
+    public abstract EntityKind childEntityKind();
+
+    public abstract Optional<LocalDateTime> attestedAt();
+    public abstract Optional<String> attestedBy();
+
+    @Nullable
+    public abstract String comments();
+}
+
---
+++ waltz-ng/client/attestation/services/attestation-instance-store.js
@@ -0,0 +1,45 @@
+export function store($http, baseApiUrl) {
+    const base = `${baseApiUrl}/attestation-instance`;
+
+    const findByUser = () => {
+        return $http
+            .get(`${base}/user`)
+            .then(result => result.data);
+    };
+
+    const attestInstance = (id, comments) => {
+        return $http
+            .post(`${base}/attest/${id}`, {
+                id,
+                comments
+            });
+    };
+
+    return {
+        findByUser,
+        attestInstance,
+    };
+}
+
+
+store.$inject = [
+    '$http',
+    'BaseApiUrl'
+];
+
+
+export const serviceName = 'AttestationInstanceStore';
+
+
+export const AttestationInstanceStore_API = {
+    findByUser: {
+        serviceName,
+        serviceFnName: 'findByUser',
+        description: 'find attestations for a user'
+    },
+    attestInstance: {
+        serviceName,
+        serviceFnName: 'attestInstance',
+        description: 'create an attestation'
+    }
+};
