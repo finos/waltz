e0c6a8bcbe0aab46434dc7aee580cb4249964c33
kamransaleem <kamran@thinkincode.co.uk>
2017-09-27 10:08:14 +0100

    Componentise App Ratings section -fix merge error
    #2609

--- waltz-ng/client/applications/app-view.js
+++ waltz-ng/client/applications/app-view.js
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 import { CORE_API } from "../common/services/core-api-utils";
-import { CORE_API } from "../common/services/core-api-utils";
 
 import template from "./app-view.html";
 
