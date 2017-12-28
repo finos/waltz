47a9c50d1c9b8d731435fd5e6ff2f36f0925153b
Dave Watkins <davidwatkins73@gmail.com>
2017-12-28 06:25:06 +0000

    WIP - display list of cloneable flows (outbounds) - started utilities collection for phys flows
    #2912

--- waltz-ng/client/physical-flows/components/register/clone/physical-flow-clone-selector.js
+++ waltz-ng/client/physical-flows/components/register/clone/physical-flow-clone-selector.js
@@ -16,21 +16,88 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 import template from './physical-flow-clone-selector.html';
+
+import _ from 'lodash';
+import {initialiseData} from "../../../../common/index";
+import {CORE_API} from "../../../../common/services/core-api-utils";
+import {sameRef} from "../../../../common/entity-utils";
+import {combinePhysicalWithLogical} from "../../../physical-flow-utils";
+
+
+const SEARCH_CUTOFF = 2;
 
 
 const bindings = {
-
+    parentEntityRef: '<',
+    onDismiss: '<',
 };
 
 
+const initialState = {
+    inbound: [],
+    outbound: [],
+    visibility: {
+        search: false
+    }
+};
 
-function controller() {
+
+function sortFlows(fs = []) {
+    const fieldOrder = ['logical.target.name', 'specification.name'];
+    return _.orderBy(fs, fieldOrder);
+}
+
+
+function controller($q,
+                    displayNameService,
+                    serviceBroker) {
+
+    const vm = initialiseData(this, initialState);
+
+    vm.$onInit = () => {
+        const logicalPromise = serviceBroker
+            .loadViewData(CORE_API.LogicalFlowStore.findByEntityReference, [ vm.parentEntityRef ])
+            .then(r => r.data);
+
+        const physicalPromise = serviceBroker
+            .loadViewData(CORE_API.PhysicalFlowStore.findByEntityReference, [ vm.parentEntityRef ])
+            .then(r => r.data);
+
+        const specPromise = serviceBroker
+            .loadViewData(CORE_API.PhysicalSpecificationStore.findByEntityReference, [ vm.parentEntityRef ])
+            .then(r => r.data);
+
+        $q.all([logicalPromise, physicalPromise, specPromise])
+            .then(([logicals, physicals, specs]) => {
+                const combined = combinePhysicalWithLogical(
+                    physicals,
+                    logicals,
+                    specs,
+                    displayNameService);
+                const [outbound, inbound] = _.partition(
+                    combined,
+                    c => sameRef(c.logical.source, vm.parentEntityRef));
+                vm.inbound = sortFlows(inbound);
+                vm.outbound = sortFlows(outbound);
+                vm.visibility.search = vm.outbound.length > SEARCH_CUTOFF;
+            });
+    };
+
+    vm.onSelect = (flow) => {
+        console.log('pfcs', { flow })
+    };
 
 }
 
 
-controller.$inject = [];
+controller.$inject = [
+    '$q',
+    'DisplayNameService',
+    'ServiceBroker'
+];
+
 
 const component = {
     controller,
