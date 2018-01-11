bad1ffcb09c8cb5cd54949cf8a832326ff383f8e
kamransaleem <kamran@thinkincode.co.uk>
2018-01-11 16:10:44 +0000

    Bulk Loader: Improve summary on logical flow parser component
    #2934

--- waltz-ng/client/logical-flow/components/bulk-logical-flow-parser/bulk-logical-flow-parser.js
+++ waltz-ng/client/logical-flow/components/bulk-logical-flow-parser/bulk-logical-flow-parser.js
@@ -37,9 +37,12 @@ const bindings = {
 
 const initialState = {
     columnMappings: {},
+    filterCriteria: null,
+    filteredData: [],
     loading: false,
     parsedData: [],
     sourceData: [],
+    summary: {},
 
     onParseComplete: (event) => console.log('default onParseComplete handler for bulk-logical-flow-parser: ', event)
 };
@@ -96,6 +99,37 @@ function findExistingLogicalFlowsAndDecorators(serviceBroker, sourcesAndTargets)
 }
 
 
+function mkParseSummary(data = []) {
+    const allParsedRefs = _.flatMap(data, d => [d.source, d.target, d.dataType]);
+    return Object.assign(
+        {
+            total: data.length,
+            newFlows: 0,
+            existingFlows: 0,
+            missingEntities: 0,
+            foundEntities: 0
+        },
+        _.countBy(data, r => r.existing == null ? 'newFlows' : 'existingFlows'),
+        _.countBy(allParsedRefs, p => p.entityRef == null ? 'missingEntities' : 'foundEntities'));
+}
+
+
+function mkFilterPredicate(criteria) {
+    switch (criteria) {
+        case 'NOT_FOUND':
+            return (r) => r.source.entityRef === null
+                || r.target.entityRef === null
+                || r.dataType.entityRef === null;
+        case 'NEW':
+            return (r) => r.existing === null;
+        case 'EXISTING':
+            return (r) => r.existing !== null;
+        default:
+            return (r) => true;
+    }
+}
+
+
 function controller($q, serviceBroker) {
     const vm = initialiseData(this, initialState);
 
@@ -121,54 +155,54 @@ function controller($q, serviceBroker) {
         return _.sumBy(allParsedRefs, p => p.entityRef == null ? 1 : 0);
     };
 
+    const filterResults = (criteria) => {
+        vm.filterCriteria = criteria;
+        return _.filter(vm.parsedData, mkFilterPredicate(criteria));
+    };
+
     const parseData = async () => {
         if(vm.columnMappings && vm.sourceData) {
             const mappedData = mapColumns(vm.columnMappings, vm.sourceData, vm.columnResolvers);
 
-            if(mappedData.length > 0 && parseErrorCount(mappedData) == 0) {
-                //compare against logical flows in database
-
-                const sourcesAndTargets = _.map(mappedData, p => ({source: p.source.entityRef, target: p.target.entityRef}));
-                const existingFlows = await findExistingLogicalFlowsAndDecorators(serviceBroker, sourcesAndTargets);
-
-                // nesting: source -> target -> data type
-                const existingFlowsNested = nest()
-                    .key(flow => refToString(flow.source))
-                    .key(flow => refToString(flow.target))
-                    .key(flow => refToString(flow.decorator.decoratorEntity))
-                    .object(existingFlows);
-
-                const parsedWithExisting = _
-                    .chain(mappedData)
-                    .map(p => {
-                        const sourceRefString = refToString(p.source.entityRef);
-                        const targetRefString = refToString(p.target.entityRef);
-                        const dataTypeString = refToString(p.dataType.entityRef);
-                        const existingFlows = _.get(existingFlowsNested, `[${sourceRefString}][${targetRefString}][${dataTypeString}]`);
-                        let existing = null;
-                        if(existingFlows && existingFlows.length > 0) {
-                            existing = existingFlows[0];
-                        }
-                        return Object.assign({}, p, {existing});
-                    })
-                    .sortBy([
-                        o => o.source.entityRef.name,
-                        o => o.target.entityRef.name,
-                        o => o.dataType.entityRef.name,
-                    ])
-                    .value();
-                vm.parsedData = parsedWithExisting;
-            } else {
-                vm.parsedData = mappedData;
+            if(mappedData.length == 0 || parseErrorCount(mappedData) > 0) {
+                return mappedData;
             }
 
+            //no parse errors - compare against logical flows in database
+            const sourcesAndTargets = _.map(mappedData, p => ({
+                source: p.source.entityRef,
+                target: p.target.entityRef
+            }));
 
-            vm.loading = false;
-            const event = {
-                data: vm.parsedData,
-                isComplete
-            };
-            invokeFunction(vm.onParseComplete, event, vm.loading)
+            const existingFlows = await findExistingLogicalFlowsAndDecorators(serviceBroker, sourcesAndTargets);
+
+            // nesting: source -> target -> data type
+            const existingFlowsNested = nest()
+                .key(flow => refToString(flow.source))
+                .key(flow => refToString(flow.target))
+                .key(flow => refToString(flow.decorator.decoratorEntity))
+                .object(existingFlows);
+
+            const parsedWithExisting = _
+                .chain(mappedData)
+                .map(p => {
+                    const sourceRefString = refToString(p.source.entityRef);
+                    const targetRefString = refToString(p.target.entityRef);
+                    const dataTypeString = refToString(p.dataType.entityRef);
+                    const existingFlows = _.get(existingFlowsNested, `[${sourceRefString}][${targetRefString}][${dataTypeString}]`);
+                    let existing = null;
+                    if (existingFlows && existingFlows.length > 0) {
+                        existing = existingFlows[0];
+                    }
+                    return Object.assign({}, p, {existing});
+                })
+                .sortBy([
+                    o => o.source.entityRef.name,
+                    o => o.target.entityRef.name,
+                    o => o.dataType.entityRef.name,
+                ])
+                .value();
+            return parsedWithExisting;
         }
     };
 
@@ -184,14 +218,31 @@ function controller($q, serviceBroker) {
                 'target': (identifier) => resolveEntityRef(entityRefsByAssetCode, 'APPLICATION', identifier),
                 'dataType': (identifier) => resolveEntityRef(dataTypesByCode, 'DATA_TYPE', identifier),
             };
-            console.log('parser initialised: ', vm.loading);
-            return parseData();
+
+            return parseData()
+                .then((data) => {
+                    vm.parsedData = data;
+                    vm.filteredData = filterResults();
+                    vm.summary = mkParseSummary(vm.parsedData);
+                    vm.loading = false;
+
+                    const event = {
+                        data: vm.parsedData,
+                        isComplete
+                    };
+                    invokeFunction(vm.onParseComplete, event, vm.loading);
+                });
         });
     };
 
 
     vm.$onChanges = async (changes) => {
 
+    };
+
+
+    vm.applyFilter = (criteria) => {
+        vm.filteredData = filterResults(criteria);
     };
 }
 
