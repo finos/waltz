aa66ea31b88eaebeeacde1cb48a3a777cfe9ea45
kamransaleem <kamran@thinkincode.co.uk>
2018-02-23 14:30:10 +0000

    Missing copyright/license terms from 3rd party authors
    #3003

--- waltz-ng/client/common/d3-context-menu.js
+++ waltz-ng/client/common/d3-context-menu.js
@@ -1,30 +1,34 @@
-/*
- * Waltz - Enterprise Architecture
- * Copyright (C) 2016, 2017 Waltz open source project
- * See README.md for more information
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-import {selectAll, select, event} from 'd3-selection';
-
 /**
  *   Adapted from: https://github.com/pkerpedjiev/d3-context-menu
+ *
+ *   The MIT License (MIT)
+ *
+ *   Copyright (C) 2014-present Patrick Gillespie and other contributors
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
  *
  *   - converted to ES6 and d3 v4
  *   - allowed actions to return a `result` object which is passed to the `onClose` callback handler
  */
+
+import {selectAll, select, event} from 'd3-selection';
+
 
 export function d3ContextMenu(menu, opts) {
     let openCallback,
