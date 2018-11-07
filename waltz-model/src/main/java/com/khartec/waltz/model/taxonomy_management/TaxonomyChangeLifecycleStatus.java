8d7aa55458cc00cb3d8cadb0fc8518290025c0a5
Dave Watkins <davidwatkins73@gmail.com>
2018-11-07 08:08:09 +0000

    Taxonomy Management: create command object
    #3545

---
+++ waltz-model/src/main/java/com/khartec/waltz/model/taxonomy_management/TaxonomyChangeLifecycleStatus.java
@@ -0,0 +1,10 @@
+package com.khartec.waltz.model.taxonomy_management;
+
+public enum TaxonomyChangeLifecycleStatus {
+
+    DRAFT,
+    ABORTED,
+    EXECUTED,
+    FAILED
+
+}
---
+++ waltz-model/src/main/java/com/khartec/waltz/model/taxonomy_management/TaxonomyChangeType.java
@@ -0,0 +1,15 @@
+package com.khartec.waltz.model.taxonomy_management;
+
+public enum TaxonomyChangeType {
+
+    ADD,
+    REMOVE,
+    UPDATE_NAME,
+    UPDATE_DESCRIPTION,
+    UPDATE_CONCRETENESS,
+    UPDATE_EXTERNAL_ID,
+    MOVE,
+    MERGE,
+    CLONE
+
+}
