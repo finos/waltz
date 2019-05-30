9f9ce2de095cb31c7ca10ceffa71516c4056fbd0
Muzammil <muzammil.ali@db.com>
2019-05-30 10:58:32 +0100

    CTCTOWALTZ-1294 rollback file

--- waltz-web/src/main/java/com/khartec/waltz/web/endpoints/auth/HeaderBasedAuthenticationFilter.java
+++ waltz-web/src/main/java/com/khartec/waltz/web/endpoints/auth/HeaderBasedAuthenticationFilter.java
@@ -57,8 +57,7 @@ public class HeaderBasedAuthenticationFilter extends WaltzFilter {
     @Override
     public void handle(Request request,
                        Response response) throws Exception {
-//        String userParam = request.headers(paramName);
-        String userParam = "muzammil.ali@db.com";
+        String userParam = request.headers(paramName);
         LOG.debug("User according to header: "+userParam);
         if (notEmpty(userParam)) {
             AuthenticationUtilities.setUser(request, userParam);
