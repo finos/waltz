8a598e0b4b5edb1b4de0afaef7e309fb7a5d194e
David Watkins <davidwatkins73@gmail.com>
2019-06-17 21:42:00 +0100

    Assessment Rating pie charts
    - improved sample data generator
    - code cleanup
    
    #4151

--- waltz-jobs/src/main/java/com/khartec/waltz/jobs/generators/SampleDataGenerator.java
+++ waltz-jobs/src/main/java/com/khartec/waltz/jobs/generators/SampleDataGenerator.java
@@ -1,5 +1,24 @@
 /*
  * Waltz - Enterprise Architecture
+ * Copyright (C) 2016, 2017, 2018, 2019 Waltz open source project
+ * See README.md for more information
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Waltz - Enterprise Architecture
  * Copyright (C) 2016  Khartec Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,8 +37,8 @@
 
 package com.khartec.waltz.jobs.generators;
 
-import org.jooq.Condition;
-import org.jooq.DSLContext;
+import org.jooq.*;
+import org.jooq.impl.DSL;
 import org.springframework.context.ApplicationContext;
 
 import java.util.List;
@@ -35,7 +54,7 @@ public interface SampleDataGenerator {
     String SAMPLE_DATA_USER = "admin";
 
     int NUM_APPS = 500;
-    int NUM_CHANGE_INITIATIVES = 4;
+    int NUM_CHANGE_INITIATIVES = 100;
     int NUM_PROCESS_GROUPS = 3;
     int NUM_PROCESSES_IN_GROUP = 6;
     int MAX_RATINGS_PER_APP = 12;
@@ -58,10 +77,24 @@ public interface SampleDataGenerator {
 
 
     default List<Long> getAppIds(DSLContext dsl) {
-        return dsl.select(APPLICATION.ID)
-                .from(APPLICATION)
-                .fetch()
-                .map(r -> r.value1());
+        return loadAllIds(dsl, APPLICATION.ID);
+    }
+
+
+    default <T> List<T> loadAllIds(DSLContext dsl,
+                                   TableField<? extends Record, T> idCol) {
+        return loadAllIds(dsl, idCol, DSL.trueCondition());
+    }
+
+
+    default <T> List<T> loadAllIds(DSLContext dsl,
+                                   TableField<? extends Record, T> idCol,
+                                   Condition condition) {
+        return dsl
+                .select(idCol)
+                .from(idCol.getTable())
+                .where(condition)
+                .fetch(idCol);
     }
 
 
@@ -74,8 +107,7 @@ public interface SampleDataGenerator {
                 .select(MEASURABLE.ID)
                 .from(MEASURABLE)
                 .where(sampleMeasurableCondition)
-                .fetch()
-                .map(r -> r.value1());
+                .fetch(MEASURABLE.ID);
 
         dsl.deleteFrom(MEASURABLE_RATING)
                 .where(MEASURABLE_RATING.MEASURABLE_ID.in(mIds));
