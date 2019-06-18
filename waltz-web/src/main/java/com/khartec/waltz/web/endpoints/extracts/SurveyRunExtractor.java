5abf2fcb579147f98c455f126995d84652f5ea01
Muzammil <muzammil.ali@db.com>
2019-06-18 13:35:21 +0100

    CTCTOWALTZ-1286 moved common fields to static

--- waltz-web/src/main/java/com/khartec/waltz/web/endpoints/extracts/SurveyRunExtractor.java
+++ waltz-web/src/main/java/com/khartec/waltz/web/endpoints/extracts/SurveyRunExtractor.java
@@ -1,5 +1,6 @@
 package com.khartec.waltz.web.endpoints.extracts;
 
+import com.khartec.waltz.common.ListUtilities;
 import com.khartec.waltz.data.InlineSelectFieldFactory;
 import com.khartec.waltz.model.EntityKind;
 import org.jooq.*;
@@ -8,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static com.khartec.waltz.schema.Tables.*;
 import static com.khartec.waltz.schema.tables.Application.APPLICATION;
@@ -20,6 +23,33 @@ import static spark.Spark.get;
 public class SurveyRunExtractor extends BaseDataExtractor {
 
     private static final Logger LOG = LoggerFactory.getLogger(SurveyRunExtractor.class);
+    private static List<Field> SURVEY_RESPONSE_FIELDS;
+    private static List<Field> SURVEY_INSTANCE_FIELDS;
+
+    static {
+        Field<String> ENTITY_RESPONSE_NAME_FIELD = InlineSelectFieldFactory.mkNameField(
+                SURVEY_QUESTION_RESPONSE.ENTITY_RESPONSE_ID,
+                SURVEY_QUESTION_RESPONSE.ENTITY_RESPONSE_KIND);
+
+        Field<String> answer = DSL.concat(SURVEY_QUESTION_RESPONSE.STRING_RESPONSE.coalesce(""),
+                SURVEY_QUESTION_RESPONSE.NUMBER_RESPONSE.cast(String.class).coalesce(""),
+                SURVEY_QUESTION_RESPONSE.BOOLEAN_RESPONSE.cast(String.class).coalesce(""),
+                SURVEY_QUESTION_RESPONSE.DATE_RESPONSE.cast(String.class).coalesce(""),
+                ENTITY_RESPONSE_NAME_FIELD.coalesce("")).as("Answer");
+
+        SURVEY_RESPONSE_FIELDS = ListUtilities.asList(
+                SURVEY_QUESTION.SECTION_NAME.as("Section"),
+                SURVEY_QUESTION.QUESTION_TEXT.as("Question"),
+                answer,
+                SURVEY_QUESTION_RESPONSE.COMMENT.as("Comment"));
+
+        SURVEY_INSTANCE_FIELDS = ListUtilities.asList(
+                SURVEY_INSTANCE.DUE_DATE.as("Due Date"),
+                SURVEY_INSTANCE.SUBMITTED_BY.as("Submitted By"),
+                SURVEY_INSTANCE.SUBMITTED_AT.as("Submitted At"),
+                SURVEY_INSTANCE.APPROVED_BY.as("Approved By"),
+                SURVEY_INSTANCE.APPROVED_AT.as("Approved At"));
+    }
 
     @Autowired
     public SurveyRunExtractor(DSLContext dsl) {
@@ -29,8 +59,8 @@ public class SurveyRunExtractor extends BaseDataExtractor {
     @Override
     public void register() {
         String surveyRunPath = mkPath("data-extract", "survey-run", ":id");
-        String surveyInstanceResponsesPath = mkPath("data-extract", "survey-run-response" , "instance", ":id");
-        String surveyRunResponsesPath = mkPath("data-extract", "survey-run-response",":id");
+        String surveyInstanceResponsesPath = mkPath("data-extract", "survey-run-response", "instance", ":id");
+        String surveyRunResponsesPath = mkPath("data-extract", "survey-run-response", ":id");
 
         get(surveyRunPath, (request, response) -> {
             long runId = getId(request);
@@ -67,38 +97,13 @@ public class SurveyRunExtractor extends BaseDataExtractor {
 
     }
 
-    private SelectSeekStep2<Record4<String, String, String, String>, Integer, String> getSurveyInstanceResponses(
-            long surveyInstanceId) {
-        Field<String> ENTITY_RESPONSE_NAME_FIELD = InlineSelectFieldFactory.mkNameField(
-                SURVEY_QUESTION_RESPONSE.ENTITY_RESPONSE_ID,
-                SURVEY_QUESTION_RESPONSE.ENTITY_RESPONSE_KIND);
-
-        return dsl.select(SURVEY_QUESTION.SECTION_NAME.as("Section"),
-                SURVEY_QUESTION.QUESTION_TEXT.as("Question"),
-                DSL.concat(SURVEY_QUESTION_RESPONSE.STRING_RESPONSE.coalesce(""),
-                        SURVEY_QUESTION_RESPONSE.NUMBER_RESPONSE.cast(String.class).coalesce(""),
-                        SURVEY_QUESTION_RESPONSE.BOOLEAN_RESPONSE.cast(String.class).coalesce(""),
-                        SURVEY_QUESTION_RESPONSE.DATE_RESPONSE.cast(String.class).coalesce(""),
-                        ENTITY_RESPONSE_NAME_FIELD.coalesce("")).as("Answer"),
-                SURVEY_QUESTION_RESPONSE.COMMENT.as("Comment"))
-                .from(SURVEY_QUESTION)
-                .join(SURVEY_QUESTION_RESPONSE)
-                .on(SURVEY_QUESTION_RESPONSE.QUESTION_ID.eq(SURVEY_QUESTION.ID))
-                .where(SURVEY_QUESTION_RESPONSE.SURVEY_INSTANCE_ID.eq(surveyInstanceId))
-                .orderBy(SURVEY_QUESTION.POSITION, SURVEY_QUESTION.QUESTION_TEXT);
-    }
-
     private SelectConditionStep<?> getSurveyRunInstances(long surveyRunId) {
         return dsl
                 .select(APPLICATION.NAME.coalesce(CHANGE_INITIATIVE.NAME).as("Entity Name"),
                         APPLICATION.ASSET_CODE.coalesce(CHANGE_INITIATIVE.EXTERNAL_ID).as("Entity Id"),
                         SURVEY_INSTANCE.ENTITY_KIND.as("Entity Kind"),
-                        SURVEY_INSTANCE.STATUS.as("Status"),
-                        SURVEY_INSTANCE.DUE_DATE.as("Due Date"),
-                        SURVEY_INSTANCE.SUBMITTED_BY.as("Submitted By"),
-                        SURVEY_INSTANCE.SUBMITTED_AT.as("Submitted At"),
-                        SURVEY_INSTANCE.APPROVED_BY.as("Approved By"),
-                        SURVEY_INSTANCE.APPROVED_AT.as("Approved At"))
+                        SURVEY_INSTANCE.STATUS.as("Status"))
+                .select(SURVEY_INSTANCE_FIELDS)
                 .from(SURVEY_INSTANCE)
                 .leftOuterJoin(APPLICATION).on(SURVEY_INSTANCE.ENTITY_KIND.eq(EntityKind.APPLICATION.name())
                         .and(APPLICATION.ID.eq(SURVEY_INSTANCE.ENTITY_ID)))
@@ -108,29 +113,26 @@ public class SurveyRunExtractor extends BaseDataExtractor {
                 .and(SURVEY_INSTANCE.ORIGINAL_INSTANCE_ID.isNull());
     }
 
+    private SelectSeekStep2<Record, Integer, String> getSurveyInstanceResponses(
+            long surveyInstanceId) {
+
+        return dsl.select(SURVEY_RESPONSE_FIELDS)
+                .from(SURVEY_QUESTION)
+                .join(SURVEY_QUESTION_RESPONSE)
+                .on(SURVEY_QUESTION_RESPONSE.QUESTION_ID.eq(SURVEY_QUESTION.ID))
+                .where(SURVEY_QUESTION_RESPONSE.SURVEY_INSTANCE_ID.eq(surveyInstanceId))
+                .orderBy(SURVEY_QUESTION.POSITION, SURVEY_QUESTION.QUESTION_TEXT);
+    }
+
     private SelectConditionStep<?> getSurveyRunWithResponses(long surveyRunId) {
-        Field<String> ENTITY_RESPONSE_NAME_FIELD = InlineSelectFieldFactory.mkNameField(
-                SURVEY_QUESTION_RESPONSE.ENTITY_RESPONSE_ID,
-                SURVEY_QUESTION_RESPONSE.ENTITY_RESPONSE_KIND);
-        return dsl
-                .select(
-                        APPLICATION.NAME.coalesce(CHANGE_INITIATIVE.NAME).as("Entity Name"),
-                        APPLICATION.ASSET_CODE.coalesce(CHANGE_INITIATIVE.EXTERNAL_ID).as("Entity Id"),
-                        SURVEY_INSTANCE.ENTITY_KIND.as("Entity Kind"),
-                        SURVEY_INSTANCE.STATUS.as("Survey Instance Status"),
-                        SURVEY_QUESTION.QUESTION_TEXT.as("Question"),
-                        DSL.concat(SURVEY_QUESTION_RESPONSE.STRING_RESPONSE.coalesce(""),
-                                SURVEY_QUESTION_RESPONSE.NUMBER_RESPONSE.cast(String.class).coalesce(""),
-                                SURVEY_QUESTION_RESPONSE.BOOLEAN_RESPONSE.cast(String.class).coalesce(""),
-                                SURVEY_QUESTION_RESPONSE.DATE_RESPONSE.cast(String.class).coalesce(""),
-                                ENTITY_RESPONSE_NAME_FIELD.coalesce("")).as("Answer"),
-                        SURVEY_QUESTION_RESPONSE.COMMENT.as("Comment"),
-                        PERSON.EMAIL.as("Participant Email"),
-                        SURVEY_INSTANCE.SUBMITTED_BY.as("Submitted By"),
-                        SURVEY_INSTANCE.SUBMITTED_AT.as("Submitted At"),
-                        SURVEY_INSTANCE.DUE_DATE.as("Due Date"),
-                        SURVEY_QUESTION_RESPONSE.LAST_UPDATED_AT.as("Last Updated At")
-                )
+        return dsl.select(
+                APPLICATION.NAME.coalesce(CHANGE_INITIATIVE.NAME).as("Entity Name"),
+                APPLICATION.ASSET_CODE.coalesce(CHANGE_INITIATIVE.EXTERNAL_ID).as("Entity Id"),
+                SURVEY_INSTANCE.ENTITY_KIND.as("Entity Kind"),
+                SURVEY_INSTANCE.STATUS.as("Status"))
+                .select(SURVEY_RESPONSE_FIELDS)
+                .select(PERSON.EMAIL.as("Participant Email"))
+                .select(SURVEY_INSTANCE_FIELDS)
                 .from(SURVEY_INSTANCE)
                 .leftOuterJoin(APPLICATION).on(SURVEY_INSTANCE.ENTITY_KIND.eq(EntityKind.APPLICATION.name())
                         .and(APPLICATION.ID.eq(SURVEY_INSTANCE.ENTITY_ID)))
