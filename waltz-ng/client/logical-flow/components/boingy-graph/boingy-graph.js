8a716fd0183635014fea489cc9b0ad30f831ea41
Constantin Mitran <constantin.mitran@db.com>
2019-07-03 10:22:13 +0100

    Boingy graph: prevent nodes flying off
    #CTCTOWALTZ-1334
    #4142

--- waltz-ng/client/logical-flow/components/boingy-graph/boingy-graph.js
+++ waltz-ng/client/logical-flow/components/boingy-graph/boingy-graph.js
@@ -1,4 +1,4 @@
-/*
+forceCenter/*
  * Waltz - Enterprise Architecture
  * Copyright (C) 2016, 2017 Waltz open source project
  * See README.md for more information
@@ -17,26 +17,26 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {initialiseData} from '../../../common';
-import {lineWithArrowPath, responsivefy} from '../../../common/d3-utils';
-import {event, select} from 'd3-selection';
-import {forceCenter, forceCollide, forceLink, forceManyBody, forceSimulation} from 'd3-force';
-import {drag} from 'd3-drag';
-import {symbol, symbolWye} from 'd3-shape';
-import {zoom, zoomIdentity} from 'd3-zoom';
-import 'd3-selection-multi';
+import {initialiseData} from "../../../common";
+import {lineWithArrowPath, responsivefy} from "../../../common/d3-utils";
+import {event, select} from "d3-selection";
+import {forceCenter, forceCollide, forceLink, forceManyBody, forceSimulation, forceX, forceY} from "d3-force";
+import {drag} from "d3-drag";
+import {symbol, symbolWye} from "d3-shape";
+import {zoom, zoomIdentity} from "d3-zoom";
+import "d3-selection-multi";
 import _ from "lodash";
 
 
-import template from './boingy-graph.html';
+import template from "./boingy-graph.html";
 
 const width = 800;
 const height = 500;
 
 
 const bindings = {
-    data: '<',
-    tweakers: '<'
+    data: "<",
+    tweakers: "<"
 };
 
 
@@ -51,13 +51,11 @@ const DEFAULT_TWEAKER = {
     update: (selection) => selection
 };
 
-
 const simulation = forceSimulation()
-    .velocityDecay(0.5)
-    .force("center", forceCenter(width / 2, height / 2))
+    .force("link", forceLink().id(d => d.id))
     .force("charge", forceManyBody().strength(-110).distanceMin(1).distanceMax(400))
-    .force("collide", forceCollide())
-    .force("link", forceLink().id(d => d.id).iterations(2).distance(55)); //.strength(0.2));
+    .force("x", forceX())
+    .force("y", forceY());
 
 const actorSymbol = symbol()
     .size(128)
@@ -72,7 +70,7 @@ function mkLinkData(flows = []) {
             target: f.target.id,
             data: f
         }))
-        .uniqBy('id')
+        .uniqBy("id")
         .value();
 }
 
@@ -85,9 +83,9 @@ function drawLinks(links = [], holder, tweakers) {
     const newLinks = linkSelection
         .enter()
         .append("path")
-        .classed('wdfd-link', true)
-        .attr('stroke', '#444')
-        .attr('stroke-opacity', 0.6)
+        .classed("wdfd-link", true)
+        .attr("stroke", "#444")
+        .attr("stroke-opacity", 0.6)
         .call(tweakers.enter);
 
     linkSelection
@@ -102,22 +100,22 @@ function drawLinks(links = [], holder, tweakers) {
 
 function addNodeLabel(selection) {
     selection
-        .append('text')
-        .attr('dx', 9)
-        .attr('dy', '.35em')
+        .append("text")
+        .attr("dx", 9)
+        .attr("dy", ".35em")
         .text(d => d.name);
 }
 
 
 function addNodeCircle(selection) {
     selection
-        .filter(d => d.kind === 'APPLICATION')
-        .append('circle')
+        .filter(d => d.kind === "APPLICATION")
+        .append("circle")
         .attr("class", "wdfd-glyph")
         .attr("r", 6);
 
     selection
-        .filter(d => d.kind === 'ACTOR')
+        .filter(d => d.kind === "ACTOR")
         .append("path")
         .attr("class", "wdfd-glyph")
         .attr("d", actorSymbol);
@@ -158,9 +156,9 @@ function drawNodes(nodes = [], holder, tweakers = DEFAULT_TWEAKER) {
 
     const newNodes = nodeSelection
         .enter()
-        .append('g')
-        .classed('wdfd-node', true)
-        .on('dblclick.unfix', d => { d.fx = null; d.fy = null })
+        .append("g")
+        .classed("wdfd-node", true)
+        .on("dblclick.unfix", d => { d.fx = null; d.fy = null })
         .call(drag()
             .on("start", dragStarted)
             .on("drag", dragged)
@@ -169,7 +167,7 @@ function drawNodes(nodes = [], holder, tweakers = DEFAULT_TWEAKER) {
     newNodes
         .call(addNodeLabel)
         .call(addNodeCircle)
-        .call(tweakers.enter)
+        .call(tweakers.enter);
 
     nodeSelection
         .exit()
@@ -187,7 +185,7 @@ function setup(vizElem) {
         .append('svg')
         .attr("width", width)
         .attr("height", height)
-        .attr('viewBox', `0 0 ${width} ${height}`);
+        .attr("viewBox", [-width / 2, -height / 2, width, height]);
 
     const destroyResizeListener = responsivefy(svg);
 
@@ -214,17 +212,17 @@ function draw(data = [],
 
     const linkSelection = drawLinks(
         links,
-        parts.svg.select('.links'),
+        parts.svg.select(".links"),
         linkTweakers);
 
     const nodeSelection = drawNodes(
         nodes,
-        parts.svg.select('.nodes'),
+        parts.svg.select(".nodes"),
         nodeTweakers);
 
     const ticked = () => {
         nodeSelection
-            .attr('transform', d => `translate(${d.x}, ${d.y})`);
+            .attr("transform", d => `translate(${d.x}, ${d.y})`);
 
         linkSelection
             .call(lineWithArrowPath);
@@ -278,9 +276,9 @@ function controller($timeout, $element) {
 
         const t = event.transform;
 
-        svg.select('.nodes')
+        svg.select(".nodes")
             .attr("transform", t);
-        svg.select('.links')
+        svg.select(".links")
             .attr("transform", t);
     }
 
@@ -298,7 +296,7 @@ function controller($timeout, $element) {
     vm.disableZoom = () => {
         vizElem
             .select("svg")
-            .on('.zoom', null);
+            .on(".zoom", null);
 
         vm.zoomEnabled = false;
     };
@@ -312,7 +310,7 @@ function controller($timeout, $element) {
         });
 
         vizElem
-            .select('svg')
+            .select("svg")
             .transition()
             .duration(750)
             .call(myZoom.transform, zoomIdentity);
@@ -322,7 +320,7 @@ function controller($timeout, $element) {
 }
 
 
-controller.$inject = ['$timeout', '$element'];
+controller.$inject = ["$timeout", "$element"];
 
 
 const component = {
