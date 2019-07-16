7b42155b8e78e30a28015dd30c72c5fa81bce9d1
Muzammil <muzammil.ali@db.com>
2019-07-16 19:27:10 +0100

    CTCTOWALTZ-1367 cleanup

--- waltz-ng/client/measurable-rating/components/browser-tree-panel/measurable-ratings-browser-tree-panel.js
+++ waltz-ng/client/measurable-rating/components/browser-tree-panel/measurable-ratings-browser-tree-panel.js
@@ -280,8 +280,8 @@ function controller($q, serviceBroker) {
 
     vm.onSelect = (measurable, categoryId) => {
         if(_.isEmpty(measurable)) {
-            vm.selectedMeasurable = {name: "Unmapped Applications",
-                description: "Display application which does not have any measurable ratings in current measurable category."};
+            vm.selectedMeasurable = { name: "Unmapped Applications",
+                description: "Display applications which do not have any associated measurable rating for this category."};
             loadUnmappedApplications(vm.measurables, categoryId);
             return;
         }
