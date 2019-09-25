aecead4978f9ce369d416d8973ba30cc7b1461e2
kamransaleem <kamran@thinkincode.co.uk>
2019-09-25 16:19:25 +0100

    Log dynamic section clicks in access log
    #2839

--- waltz-ng/client/dynamic-section/services/dynamic-section-manager.js
+++ waltz-ng/client/dynamic-section/services/dynamic-section-manager.js
@@ -88,6 +88,12 @@ const sectionManagerSvc = {
                 mkStorageKey(),
                 _.map(toActivate, s => s.id));
 
+            // log component activations to access log
+            _.chain(toActivate)
+                .map(s => `${$state.current.name}|${s.componentId}`)
+                .forEach(state => accessLogStore.write(state, $stateParams))
+                .value();
+
             blat(active, toActivate);
         }
 
@@ -103,8 +109,8 @@ const sectionManagerSvc = {
 
 
         function activate(d) {
-            const state = `${$state.current.name}|${d.componentId}`;
-            const infoPromise = accessLogStore.write(state, $stateParams);
+            accessLogStore.write(`${$state.current.name}|${d.componentId}`, $stateParams);
+
             blat(
                 active,
                 _.concat([d], _.without(active, d)));
