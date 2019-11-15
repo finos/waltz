c0d1699a1d6284a2060ca4da751d8be92142f0f2
woodjes <jessica.woodland-scott@db.com>
2019-11-15 13:46:38 +0000

    boingy graph improvements
    #CTCTOWALTZ-1570
    #4488

--- waltz-ng/client/logical-flow/components/boingy-graph/boingy-graph.js
+++ waltz-ng/client/logical-flow/components/boingy-graph/boingy-graph.js
@@ -1,19 +1,4 @@
-import {refToString} from "../../../common/entity-utils";
-import {initialiseData} from "../../../common";
-import {lineWithArrowPath, responsivefy} from "../../../common/d3-utils";
-import {event, select} from "d3-selection";
-import {forceCenter, forceLink, forceManyBody, forceSimulation, forceX, forceY} from "d3-force";
-import {drag} from "d3-drag";
-import {symbol, symbolWye} from "d3-shape";
-import {zoom, zoomIdentity} from "d3-zoom";
-import "d3-selection-multi";
-import _ from "lodash";
-
-import {scalePow} from "d3-scale";
-
-import template from "./boingy-graph.html";
-
-forceCenter/*
+/*
  * Waltz - Enterprise Architecture
  * Copyright (C) 2016, 2017 Waltz open source project
  * See README.md for more information
@@ -31,6 +16,22 @@ forceCenter/*
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+import {initialiseData} from "../../../common";
+
+import {lineWithArrowPath, responsivefy} from "../../../common/d3-utils";
+import {event, select} from "d3-selection";
+import {forceLink, forceManyBody, forceSimulation, forceX, forceY} from "d3-force";
+import {drag} from "d3-drag";
+import {symbol, symbolWye} from "d3-shape";
+import {zoom, zoomIdentity} from "d3-zoom";
+import "d3-selection-multi";
+import _ from "lodash";
+import {scalePow} from "d3-scale";
+
+import {refToString} from "../../../common/entity-utils";
+
+import template from "./boingy-graph.html";
 
 const width = 800;
 const height = 500;
