c0698fcaab3df739cae84dd091041b9e6362c069
David Watkins <davidwatkins73@gmail.com>
2019-11-21 09:42:40 +0000

    More svelte experimentation

--- waltz-ng/client/playpen/1/TestWidget.svelte
+++ waltz-ng/client/playpen/1/TestWidget.svelte
@@ -1,21 +1,26 @@
 <script>
     import {CORE_API} from "../../common/services/core-api-utils";
 
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
     export let serviceBroker;
     export let primaryEntityRef;
 
-    let bookmarks = [];
-
     const load = async (ref) => {
         console.log("Loading", { ref });
+        await sleep(500);
         const r = await serviceBroker
             .loadViewData(
                 CORE_API.BookmarkStore.findByParent,
                 [ref]);
-        bookmarks = await r.data;
+
+
+        return r.data;
     };
 
-    $: load(primaryEntityRef);
+    $: promise = load(primaryEntityRef);
 
 </script>
 
@@ -29,13 +34,12 @@
 
 <h1>Hello2!</h1>
 
-<p>
-    {console.log(bookmarks) || bookmarks.length}
-</p>
-
-
-<ul>
-    {#each bookmarks as bookmark, idx}
-        <li>{idx} / {bookmark.kind} / {bookmark.title}</li>
-    {/each}
-</ul>
+{#await promise}
+    Loading
+{:then bookmarks}
+    <ul>
+        {#each bookmarks as bookmark, idx}
+            <li>{idx} / {bookmark.kind} / {bookmark.title}</li>
+        {/each}
+    </ul>
+{/await}
