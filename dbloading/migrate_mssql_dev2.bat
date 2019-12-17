18a9e6232c45fdad6e2207afc64d2b7914179ef5
woodjes <jessica.woodland-scott@db.com>
2019-12-17 15:12:44 +0000

    #CTCTOWALTZ-1591 #4548

--- dbloading/migrate_mssql_dev2.bat
+++
@@ -1,8 +0,0 @@
-C:/Users/woodjes/tools/liquibase-3.7.0-bin/liquibase.bat ^
---driver=com.microsoft.sqlserver.jdbc.SQLServerDriver ^
---classpath=C:/Users/woodjes/.m2/repository/com/microsoft/sqlserver/sqljdbc4/4.0.2206.100_enu/sqljdbc4-4.0.2206.100_enu.jar ^
---changeLogFile=../waltz-data/src/main/ddl/liquibase/db.changelog-master.xml ^
---url="jdbc:sqlserver://LONCGDBU0021:1433;databaseName=WALTZ_DEV" ^
---username=WALTZ_USER ^
---password=WaltzU@123 ^
-migrate
