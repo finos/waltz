640d80afe8da641ce21015c2922c42c6ce39b095
Vaishnavi Gharote <vaishnavi.gharote@db.com>
2021-02-26 17:00:57 +0530

    [CTCTOWALTZ-2118]-coder to sort measurable rating pie chart and table added.

--- waltz-ng/client/measurable-rating/components/explorer-section/measurable-rating-explorer-section.js
+++ waltz-ng/client/measurable-rating/components/explorer-section/measurable-rating-explorer-section.js
@@ -64,7 +64,7 @@ function preparePie(ratings = [],
         .map(schemeItem => ({
             key: schemeItem.rating,
             count: counts[schemeItem.rating] || 0
-        }))
+        })).orderBy('count','asc')
         .value();
 
     return {
