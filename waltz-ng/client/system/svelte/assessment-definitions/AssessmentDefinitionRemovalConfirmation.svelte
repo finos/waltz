26709cb0a54c79746983807daacac649cac9cb7b
David Watkins <davidwatkins73@gmail.com>
2021-03-26 14:03:11 +0000

    wip for assessment definition editor
    - cascading delete into ratings
    
    #5350

--- waltz-ng/client/system/svelte/assessment-definitions/AssessmentDefinitionRemovalConfirmation.svelte
+++ waltz-ng/client/system/svelte/assessment-definitions/AssessmentDefinitionRemovalConfirmation.svelte
@@ -5,43 +5,101 @@
     export let doCancel;
     export let doRemove;
 
+    let canRemove = false;
+
     $: ratingCall = assessmentRatingStore.findByDefinitionId(definition.id);
     $: ratings = $ratingCall.data;
+    $: canRemove = ratings.length === 0;
 
+    function onRemove() {
+        doRemove(definition.id);
+    }
 </script>
 
-<div class="removal-warning">
-    <h3>Confirm assessment definition removal</h3>
-    Are you sure you want to remove this assessment definition ?
+{#if $ratingCall.status === 'loading'}
+    <h4 class="text-muted">
+        Analyzing impact of removing {definition.name}...
+    </h4>
+{:else}
+    <div class="removal-box"
+         class:removal-warning={ratings.length === 0}
+         class:removal-error={ratings.length > 0}>
+        <h3>Confirm assessment definition removal</h3>
+        Are you sure you want to remove this assessment definition ?
 
-    <div>
-        <h4>{definition.name}</h4>
-        <p>{definition.description}</p>
+        <div>
+            <h4>{definition.name}</h4>
+            <p>{definition.description}</p>
+        </div>
+
+        {#if ratings.length > 0}
+            <div class="reconsider">
+                There are {ratings.length} ratings associated with this definition.
+                <br>
+                If you proceed these will be lost.
+                <br>
+                Please ensure you have a backup of the data before proceeding.
+                <br>
+                You can obtain a backup from the export on the
+                <a href="../../assessment-definition/{definition.id}">overview page</a>.
+                <br>
+                <br>
+                <button class="btn btn-danger"
+                        title="Confirm removal"
+                        on:click={() => canRemove = true}>
+                    I understand
+                </button>
+                {#if canRemove}
+                    <p style="margin-top: 0.5em">
+                        Removal button (below) is now enabled
+                    </p>
+                {/if}
+
+            </div>
+        {/if}
+
+        <button class="btn"
+                class:btn-warning={ratings.length === 0}
+                class:btn-danger={ratings.length > 0}
+                disabled={!canRemove}
+                title="Remove definition"
+                on:click={onRemove}>
+            Remove
+        </button>
+        <button class="btn btn-link"
+                on:click={() => doCancel()}>
+            Cancel
+        </button>
     </div>
-
-    {#if ratings.length > 0}
-        Aargh!!!
-    {/if}
-
-    <button class="btn btn-warning"
-            on:click={() => doRemove(definition.id)}>
-        Remove
-    </button>
-    <button class="btn btn-link"
-            on:click={() => doCancel()}>
-        Cancel
-    </button>
-</div>
-
+{/if}
 
 <style>
-    .removal-warning {
-        border: 1px solid #D9923F;
+    .removal-box{
+        border-width: 1px;
+        border-style: solid;
         background-color: #faf8e9;
         padding-left: 2em;
         padding-right: 2em;
         padding-bottom: 1.5em;
         border-radius: 2px;
+
+    }
+    .removal-warning {
+        border-color: #D9923F;
+        background-color: #faf8e9;
+    }
+    .removal-error {
+        border-color: #d93f44;
+        background-color: #fae9ee;
+    }
+
+    .reconsider {
+        border: 2px solid black;
+        padding: 1em;
+        margin-bottom: 1em;
+        margin-top: 0.5em;
+        font-weight: bolder;
+        background-color: #fd9090;
     }
 </style>
 
