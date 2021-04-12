53baf9e3bdf3c357e8c8a22af98f5d77c6b42cad
David Watkins <davidwatkins73@gmail.com>
2021-04-12 16:19:40 +0100

    Embedding issue was caused by timing problems (we think)
    - took opportunity to create loading placeholder widget
    - ensured remote cache is cleared on page transitions
    
    #5372

---
+++ waltz-ng/client/common/svelte/LoadingPlaceholder.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Icon from "./Icon.svelte";
+</script>
+
+<h3 class="text-muted">
+    <Icon name="circle-o-notch"
+          spin="true"/>
+    <slot>
+        Loading ...
+    </slot>
+</h3>
