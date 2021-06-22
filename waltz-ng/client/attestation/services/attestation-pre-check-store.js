22008f73e659ea7abe95991908aced71bce01f18
david.watkins@db.com <david.watkins@db.com>
2021-06-22 16:54:47 +0100

    Attestation enhancements
    - front end uses new data backend
    
    #CTCTOWALTZ-2253
    #5469

---
+++ waltz-ng/client/attestation/services/attestation-pre-check-store.js
@@ -0,0 +1,62 @@
+/*
+ * Waltz - Enterprise Architecture
+ * Copyright (C) 2016, 2017, 2018, 2019 Waltz open source project
+ * See README.md for more information
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific
+ *
+ */
+
+import {checkIsEntityRef} from "../../common/checks";
+
+export function store($http, baseApiUrl) {
+    const base = `${baseApiUrl}/attestation-pre-check`;
+
+
+
+    const logicalFlowCheck = (ref) => {
+        checkIsEntityRef(ref);
+
+        return $http
+            .get(`${base}/logical-flow/entity/${ref.kind}/${ref.id}`)
+            .then(r => r.data);
+    };
+
+
+    return {
+        logicalFlowCheck
+    };
+}
+
+
+store.$inject = [
+    '$http',
+    'BaseApiUrl'
+];
+
+
+const serviceName = 'AttestationPreCheckStore';
+
+
+export const AttestationPreCheckStore_API = {
+    logicalFlowCheck: {
+        serviceName,
+        serviceFnName: 'logicalFlowCheck',
+        description: 'logicalFlowCheck for a given entity [ref]'
+    }
+};
+
+
+export default {
+    serviceName,
+    store
+};
