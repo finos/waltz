8d695a01afa627f803285805ca14eec8795f7c28
David Watkins <davidwatkins73@gmail.com>
2021-06-28 20:50:13 +0100

    First click selects node
    #5455

--- waltz-ng/client/flow-diagram/components/diagram-svelte/drag-handler.js
+++ waltz-ng/client/flow-diagram/components/diagram-svelte/drag-handler.js
@@ -1,18 +1,9 @@
-import {event, select} from "d3-selection";
+import {event} from "d3-selection";
 import {drag} from "d3-drag";
 import {positions} from "./store/layout";
 
 
 export function mkDragHandler(node) {
-    let dragStartPos = null;
-
-    function dragStarted() {
-        dragStartPos = { x: event.x, y: event.y };
-        return select(this)
-            .raise()
-            .classed("wfd-active", true);
-    }
-
 
     function dragger() {
         return (d) => {
@@ -20,20 +11,8 @@ export function mkDragHandler(node) {
         };
     }
 
-    function dragEnded(d) {
-        const noMove = dragStartPos.x === event.x && dragStartPos.y === event.y;
-        if (noMove) {
-            console.log("No move")
-        }
-
-        return select(this)
-            .classed("wfd-active", false);
-    }
-
     return drag()
-        .on("start.foo", dragStarted)
-        .on("drag.foo", dragger()) //commandProcessor))
-        .on("end.foo", dragEnded);
+        .on("drag", dragger());
 }
 
 
