dd71b7389debe355225a926dbfbe277c96963397
woodjes <jessica.woodland-scott@db.com>
2021-07-08 11:51:07 +0100

    Flow diagrams can have related datatypes
    #CTCTOWALTZ-2272
    #5517

--- waltz-ng/client/flow-diagram/components/diagram-svelte/context-panel/RelatedEntitiesViewTable.svelte
+++ waltz-ng/client/flow-diagram/components/diagram-svelte/context-panel/RelatedEntitiesViewTable.svelte
@@ -11,6 +11,7 @@
     export let canEdit;
     export let measurables;
     export let changeInitiatives;
+    export let datatypes;
 
     let dispatch = createEventDispatcher();
 
@@ -25,99 +26,137 @@
 
 </script>
 
-<strong>Measurables:</strong>
-<table class="table table-condensed small">
-    <!-- IF NONE -->
-    {#if _.isEmpty(measurables)}
-    <tr>
-        <td colspan="2">No associated viewpoints</td>
-    </tr>
-    {:else}
-    <thead>
-        <th width="80%"/>
-        <th width="20%"/>
-    </thead>
-    <!-- LIST -->
-    <tbody>
-    {#each measurables as measurable}
-    <tr>
-        <td>
-            <div><EntityLink ref={measurable.data}/></div>
-            <div class="small text-muted">
-                {_.get(measurable.category, "name", "unknown")}
-            </div>
-        </td>
-        <td>
-            {#if canEdit}
-            <button on:click={() => removeEntity(measurable)}
-               class="clickable">
-                <Icon name="trash"/>Remove
-            </button>
+<div style="padding-bottom: 1em">
+    <strong>Measurables:</strong>
+    <div class:waltz-scroll-region-250={_.size(measurables) > 4}>
+        <table class="table table-condensed small"
+               style="margin-bottom: 0; border-bottom: solid #ccc 1px" >
+            <!-- IF NONE -->
+            {#if _.isEmpty(measurables)}
+                <tr>
+                    <td colspan="2">No associated viewpoints</td>
+                </tr>
+            {:else}
+                <thead>
+                <th width="80%"/>
+                <th width="20%"/>
+                </thead>
+                <!-- LIST -->
+                <tbody>
+                {#each measurables as measurable}
+                    <tr>
+                        <td>
+                            <div><EntityLink ref={measurable.data}/></div>
+                            <div class="small text-muted">
+                                {_.get(measurable.category, "name", "unknown")}
+                            </div>
+                        </td>
+                        <td>
+                            {#if canEdit}
+                                <button on:click={() => removeEntity(measurable)}
+                                        class="clickable">
+                                    <Icon name="trash"/>Remove
+                                </button>
+                            {/if}
+                        </td>
+                    </tr>
+                {/each}
+                </tbody>
             {/if}
-        </td>
-    </tr>
-    {/each}
-    </tbody>
-    {/if}
-
-    <!-- FOOTER-->
+        </table>
+    </div>
     {#if canEdit}
-    <tfoot>
-    <tr>
-        <td colspan="2">
-            <button class="btn btn-skinny"
-                    on:click={() => addEntity("MEASURABLE")}>
-                <Icon name="plus"/>Add
-            </button>
-        </td>
-    </tr>
-    </tfoot>
+        <button class="btn btn-skinny"
+                on:click={() => addEntity("MEASURABLE")}>
+            <Icon name="plus"/>Add
+        </button>
     {/if}
-</table>
+</div>
 
-<strong>Change Initiatives:</strong>
-<table class="table table-condensed small">
-    <!-- IF NONE -->
-    {#if _.isEmpty(changeInitiatives)}
-        <tr>
-            <td colspan="2">No associated change initiatives</td>
-        </tr>
-    {:else}
-        <thead>
+<div style="padding-bottom: 1em;">
+    <strong>Change Initiatives:</strong>
+    <table class="table table-condensed small"
+           style="margin-bottom: 0; border-bottom: solid #ccc 1px">
+        <!-- IF NONE -->
+        {#if _.isEmpty(changeInitiatives)}
+            <tr>
+                <td colspan="2">No associated change initiatives</td>
+            </tr>
+        {:else}
+            <thead>
             <th width="80%"/>
             <th width="20%"/>
-        </thead>
-        <!-- LIST -->
-        <tbody>
-        {#each changeInitiatives as changeInitiative}
-            <tr>
-                <td>
-                    <div><EntityLink ref={changeInitiative.data}/></div>
-                </td>
-                <td>
-                    {#if canEdit}
-                    <button on:click={() => removeEntity(changeInitiative)}
-                            class="clickable">
-                        <Icon name="trash"/>Remove
-                    </button>
-                    {/if}
-                </td>
-            </tr>
-        {/each}
-        </tbody>
-    {/if}
-
-    <!-- FOOTER-->
+            </thead>
+            <!-- LIST -->
+            <tbody>
+            {#each changeInitiatives as changeInitiative}
+                <tr>
+                    <td>
+                        <div><EntityLink ref={changeInitiative.data}/></div>
+                    </td>
+                    <td>
+                        {#if canEdit}
+                            <button on:click={() => removeEntity(changeInitiative)}
+                                    class="clickable">
+                                <Icon name="trash"/>Remove
+                            </button>
+                        {/if}
+                    </td>
+                </tr>
+            {/each}
+            </tbody>
+        {/if}
+    </table>
     {#if canEdit}
-    <tfoot>
-    <tr>
-        <td colspan="2">
-            <button class="btn btn-skinny"
-                    on:click={() => addEntity("CHANGE_INITIATIVE")}>
-                <Icon name="plus"/>Add
-            </button>
-        </td>
-    </tr>
-    </tfoot>
+        <button class="btn btn-skinny"
+                on:click={() => addEntity("CHANGE_INITIATIVE")}>
+            <Icon name="plus"/>Add
+        </button>
     {/if}
-</table>
+</div>
+
+
+<div style="padding-bottom: 1em">
+    <strong>Datatypes:</strong>
+    <div class:waltz-scroll-region-250={_.size(datatypes) > 4}>
+        <table class="table table-condensed small"
+               style="margin-bottom: 0; border-bottom: solid #ccc 1px" >
+            <!-- IF NONE -->
+            {#if _.isEmpty(datatypes)}
+                <tr>
+                    <td colspan="2">No associated datatypes</td>
+                </tr>
+            {:else}
+                <thead>
+                <th width="80%"/>
+                <th width="20%"/>
+                </thead>
+                <!-- LIST -->
+                <tbody>
+                {#each datatypes as datatype}
+                    <tr>
+                        <td>
+                            <div><EntityLink ref={datatype.data}/></div>
+                        </td>
+                        <td>
+                            {#if canEdit}
+                                <button on:click={() => removeEntity(datatype)}
+                                        class="clickable">
+                                    <Icon name="trash"/>Remove
+                                </button>
+                            {/if}
+                        </td>
+                    </tr>
+                {/each}
+                </tbody>
+            {/if}
+        </table>
+    </div>
+    {#if canEdit}
+        <button class="btn btn-skinny"
+                on:click={() => addEntity("DATA_TYPE")}>
+            <Icon name="plus"/>Add
+        </button>
+    {/if}
+</div>
+
