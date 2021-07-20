122b5b78ddf875c4f5db80016d52feb95792c2ba
David Watkins <davidwatkins73@gmail.com>
2021-07-20 12:07:06 +0100

    Auth source exports
    - removed entity level extractors (see #5537)
    
    #5529

--- waltz-web/src/main/java/com/khartec/waltz/web/endpoints/extracts/AuthoritativeSourceExtractor.java
+++ waltz-web/src/main/java/com/khartec/waltz/web/endpoints/extracts/AuthoritativeSourceExtractor.java
@@ -1,15 +1,13 @@
 package com.khartec.waltz.web.endpoints.extracts;
 
 
-import com.khartec.waltz.data.orgunit.OrganisationalUnitIdSelectorFactory;
 import com.khartec.waltz.model.EntityKind;
-import com.khartec.waltz.model.EntityLifecycleStatus;
-import com.khartec.waltz.model.IdSelectionOptions;
 import com.khartec.waltz.schema.tables.Application;
-import org.jooq.*;
+import org.jooq.Condition;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.SelectHavingStep;
 import org.jooq.impl.DSL;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,16 +15,11 @@ import static com.khartec.waltz.schema.Tables.*;
 import static com.khartec.waltz.schema.tables.AuthoritativeSource.AUTHORITATIVE_SOURCE;
 import static com.khartec.waltz.schema.tables.DataType.DATA_TYPE;
 import static com.khartec.waltz.web.WebUtilities.mkPath;
-import static com.khartec.waltz.web.WebUtilities.readIdSelectionOptionsFromBody;
-import static spark.Spark.post;
 import static spark.Spark.get;
 
 @Service
 public class AuthoritativeSourceExtractor extends DirectQueryBasedDataExtractor{
 
-    private static final Logger LOG = LoggerFactory.getLogger(AuthoritativeSourceExtractor.class);
-    private final OrganisationalUnitIdSelectorFactory organisationalUnitIdSelectorFactory = new OrganisationalUnitIdSelectorFactory();
-    public final static Application CONSUMER_APP = Application.APPLICATION.as("consumer");
     public final static Application SUPPLIER_APP = Application.APPLICATION.as("supplier");
 
 
@@ -38,29 +31,6 @@ public class AuthoritativeSourceExtractor extends DirectQueryBasedDataExtractor{
 
     @Override
     public void register() {
-        post(mkPath("data-extract", "authoritative-source", "by-selector"), (request, response) -> {
-            IdSelectionOptions idSelectionOptions = readIdSelectionOptionsFromBody(request);
-            Select<Record1<Long>> orgUnitSelector = organisationalUnitIdSelectorFactory.apply(idSelectionOptions);
-            Condition condition =
-                    AUTHORITATIVE_SOURCE.ID.in(
-                      dsl.select(AUTHORITATIVE_SOURCE.ID)
-                        .from(AUTHORITATIVE_SOURCE)
-                        .innerJoin(LOGICAL_FLOW).on(LOGICAL_FLOW.SOURCE_ENTITY_ID.eq(AUTHORITATIVE_SOURCE.APPLICATION_ID)
-                          .and(LOGICAL_FLOW.SOURCE_ENTITY_KIND.eq(EntityKind.APPLICATION.name()))
-                          .and(LOGICAL_FLOW.ENTITY_LIFECYCLE_STATUS.ne(EntityLifecycleStatus.REMOVED.name())))
-                        .innerJoin(CONSUMER_APP).on(CONSUMER_APP.ID.eq(LOGICAL_FLOW.TARGET_ENTITY_ID)
-                           .and(LOGICAL_FLOW.TARGET_ENTITY_KIND.eq(EntityKind.APPLICATION.name())))
-                      .where(AUTHORITATIVE_SOURCE.PARENT_ID.in(orgUnitSelector)));
-
-            SelectHavingStep <Record> qry = prepareExtractQuery(condition);
-            String fileName = String.format(
-                    "authoritative-sources-for-%s-%s",
-                    idSelectionOptions.entityReference().kind().name().toLowerCase(),
-                    idSelectionOptions.entityReference().id());
-            LOG.debug("extracted authoritative sources for entity ref {}", idSelectionOptions.entityReference());
-            return writeExtract(fileName, qry, request, response);
-        });
-
         get(mkPath("data-extract", "authoritative-source", "all"), (request, response) -> {
             SelectHavingStep <Record> qry = prepareExtractQuery(DSL.trueCondition());
             return writeExtract("authoritative-sources", qry, request, response);
