6e55918e00c4213912c70764c5b398fa4fe18f3b
woodjes <jessica.woodland-scott@db.com>
2021-10-21 17:00:38 +0100

    Phys flow bulk upload
    checking for null values
    
    #CTCTOWALTZ-2334
    #5696

--- waltz-model/src/main/java/com/khartec/waltz/model/Wrapper.java
+++ waltz-model/src/main/java/com/khartec/waltz/model/Wrapper.java
@@ -21,11 +21,10 @@ public abstract class Wrapper<T> {
 
     @Override
     public String toString() {
-        return value().toString();
+        if(value() == null){
+            return null;
+        } else {
+            return value().toString();
+        }
     }
-
-    public String toNiceName() {
-        return getClass().getSimpleName() + "(" + value() + ")";
-    }
-
 }
