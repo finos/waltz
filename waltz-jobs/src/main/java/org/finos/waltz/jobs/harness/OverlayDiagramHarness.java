1093c95111b16ec3fdb51e04da67818f3f9ec2d0
woodjes <jessica.woodland-scott@db.com>
2022-05-30 09:29:09 +0100

    filter params WIP
    #CTCTOWALTZ-2506
    #6081

--- waltz-jobs/src/main/java/org/finos/waltz/jobs/harness/OverlayDiagramHarness.java
+++ waltz-jobs/src/main/java/org/finos/waltz/jobs/harness/OverlayDiagramHarness.java
@@ -18,27 +18,30 @@
 
 package org.finos.waltz.jobs.harness;
 
-import org.finos.waltz.common.DateTimeUtilities;
+import org.finos.waltz.common.SetUtilities;
+import org.finos.waltz.data.GenericSelector;
+import org.finos.waltz.data.GenericSelectorFactory;
 import org.finos.waltz.data.aggregate_overlay_diagram.AppCountWidgetDao;
 import org.finos.waltz.data.aggregate_overlay_diagram.TargetAppCostWidgetDao;
 import org.finos.waltz.data.application.ApplicationIdSelectorFactory;
+import org.finos.waltz.model.AssessmentBasedSelectionFilter;
 import org.finos.waltz.model.EntityKind;
 import org.finos.waltz.model.IdSelectionOptions;
-import org.finos.waltz.model.aggregate_overlay_diagram.overlay.CountWidgetDatum;
-import org.finos.waltz.model.aggregate_overlay_diagram.overlay.TargetCostWidgetDatum;
+import org.finos.waltz.model.ImmutableAssessmentBasedSelectionFilter;
 import org.finos.waltz.service.DIConfiguration;
-import org.jooq.DSLContext;
-import org.jooq.Record1;
-import org.jooq.Select;
+import org.jooq.*;
+import org.jooq.impl.DSL;
 import org.jooq.tools.json.ParseException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.time.LocalDate;
-import java.util.Set;
+import java.util.Optional;
 
+import static java.lang.String.format;
 import static org.finos.waltz.common.FunctionUtilities.time;
 import static org.finos.waltz.model.EntityReference.mkRef;
 import static org.finos.waltz.model.IdSelectionOptions.mkOpts;
+import static org.finos.waltz.schema.Tables.ASSESSMENT_RATING;
+import static org.finos.waltz.schema.Tables.CHANGE_INITIATIVE;
 
 
 public class OverlayDiagramHarness {
@@ -57,24 +60,61 @@ public class OverlayDiagramHarness {
 
         ApplicationIdSelectorFactory appSelector = new ApplicationIdSelectorFactory();
 
-        Select<Record1<Long>> appIds = appSelector.apply(ou);
 
-        LocalDate targetStateDate = DateTimeUtilities.nowUtc().toLocalDate().plusYears(1);
+        AssessmentBasedSelectionFilter assessmentFilter = ImmutableAssessmentBasedSelectionFilter.builder()
+                .definitionId(93L)
+                .ratingIds(SetUtilities.asSet(381L))
+                .build();
 
-        Set<CountWidgetDatum> appCountWidgetData = time("countWidgetData", () -> countWidgetDao.findWidgetData(
-                1,
-                appIds,
-                targetStateDate));
-
-        Set<TargetCostWidgetDatum> appCostWidgetData = time("costWidgetData", () -> costWidgetDao.findWidgetData(
-                1,
-                appIds,
-                targetStateDate));
-
-        System.out.println(appCountWidgetData);
-        System.out.println(appCostWidgetData);
+        Select<Record1<Long>> selector = prepareFilteredSelection(EntityKind.CHANGE_INITIATIVE, ou, Optional.of(assessmentFilter));
+        Select<Record1<Long>> selector2 = prepareFilteredSelection(EntityKind.CHANGE_INITIATIVE, ou, Optional.empty());
 
 
+        int changeInitiativeCount = dsl.fetchCount(DSL
+                .select()
+                .from(CHANGE_INITIATIVE)
+                .where(CHANGE_INITIATIVE.ID.in(selector)));
+
+
+        SelectConditionStep<Record> qry = dsl
+                .select()
+                .from(CHANGE_INITIATIVE)
+                .where(CHANGE_INITIATIVE.ID.in(selector));
+
+        SelectConditionStep<Record> qry2 = dsl
+                .select()
+                .from(CHANGE_INITIATIVE)
+                .where(CHANGE_INITIATIVE.ID.in(selector2));
+
+        int changeInitiativeCount2 = dsl.fetchCount(qry2);
+
+        System.out.println(format("with filter: %d", changeInitiativeCount));
+        System.out.println(qry);
+        System.out.println(changeInitiativeCount2);
+    }
+
+
+    private static Select<Record1<Long>> prepareFilteredSelection(EntityKind targetKind,
+                                                                  IdSelectionOptions options,
+                                                                  Optional<AssessmentBasedSelectionFilter> params) {
+
+        GenericSelectorFactory genericSelectorFactory = new GenericSelectorFactory();
+        GenericSelector genericSelector = genericSelectorFactory.applyForKind(targetKind, options);
+
+        return params
+                .map(p -> genericSelector.selector().intersect(mkAssessmentRatingSelector(p, genericSelector.kind())))
+                .orElse(genericSelector.selector());
+    }
+
+
+    private static Select<? extends Record1<Long>> mkAssessmentRatingSelector(AssessmentBasedSelectionFilter params,
+                                                                              EntityKind targetKind) {
+        return DSL
+                .select(ASSESSMENT_RATING.ENTITY_ID)
+                .from(ASSESSMENT_RATING)
+                .where(ASSESSMENT_RATING.ASSESSMENT_DEFINITION_ID.eq(params.definitionId())
+                        .and(ASSESSMENT_RATING.RATING_ID.in(params.ratingIds())
+                                .and(ASSESSMENT_RATING.ENTITY_KIND.eq(targetKind.name()))));
     }
 
 }
