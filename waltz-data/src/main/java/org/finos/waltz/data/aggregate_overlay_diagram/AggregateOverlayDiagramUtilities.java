472a97328956dd9e68b91791b99e1e30e6e95622
woodjes <jessica.woodland-scott@db.com>
2022-06-07 12:03:39 +0100

    Switch to new selector for backing cell to aggregated entities map
    #CTCTOWALTZ-2515
    #6085

--- waltz-data/src/main/java/org/finos/waltz/data/aggregate_overlay_diagram/AggregateOverlayDiagramUtilities.java
+++ waltz-data/src/main/java/org/finos/waltz/data/aggregate_overlay_diagram/AggregateOverlayDiagramUtilities.java
@@ -1,28 +1,30 @@
 
 package org.finos.waltz.data.aggregate_overlay_diagram;
 
-import org.finos.waltz.data.GenericSelector;
-import org.finos.waltz.data.GenericSelectorFactory;
-import org.finos.waltz.data.application.ApplicationIdSelectorFactory;
 import org.finos.waltz.model.EntityKind;
 import org.finos.waltz.model.EntityReference;
-import org.finos.waltz.model.IdSelectionOptions;
 import org.finos.waltz.model.aggregate_overlay_diagram.AggregateOverlayDiagram;
 import org.finos.waltz.model.aggregate_overlay_diagram.ImmutableAggregateOverlayDiagram;
 import org.finos.waltz.schema.tables.records.AggregateOverlayDiagramRecord;
 import org.jooq.*;
 import org.jooq.impl.DSL;
-import org.jooq.lambda.tuple.Tuple3;
+import org.jooq.lambda.tuple.Tuple2;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static java.lang.String.format;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptySet;
 import static java.util.stream.Collectors.*;
 import static org.finos.waltz.common.DateTimeUtilities.toLocalDateTime;
+import static org.finos.waltz.common.MapUtilities.groupBy;
+import static org.finos.waltz.common.SetUtilities.fromCollection;
 import static org.finos.waltz.data.JooqUtilities.readRef;
-import static org.finos.waltz.model.IdSelectionOptions.mkOpts;
 import static org.finos.waltz.schema.Tables.*;
+import static org.finos.waltz.schema.tables.EntityRelationship.ENTITY_RELATIONSHIP;
 import static org.jooq.lambda.tuple.Tuple.tuple;
 
 public class AggregateOverlayDiagramUtilities {
@@ -42,84 +44,8 @@ public class AggregateOverlayDiagramUtilities {
     };
 
 
-    protected static Map<String, Set<Long>> fetchAndGroupEntityIdsByCellId(DSLContext dsl,
-                                                                           Select<Record2<String, Long>> cellExtIdWithEntityIdSelector) {
-
-        Result<Record2<String, Long>> fetch = dsl.selectQuery(cellExtIdWithEntityIdSelector).fetch();
-
-        return dsl
-                .selectQuery(cellExtIdWithEntityIdSelector)
-                .fetchSet(r -> tuple(r.get(0, String.class), r.get(1, Long.class)))
-                .stream()
-                .collect(groupingBy(
-                        k -> k.v1,
-                        mapping(t -> t.v2, toSet())));
-    }
-
-
-    /**
-     * Returns a selector which returns cell ext ids and app ids.
-     * The result (when executed) would look like:
-     *
-     *   cellExtId | appId
-     *   ---       | ---
-     *   AFC       | 45
-     *   AFC       | 975
-     *   FINANCE   | 435
-     *   FINANCE   | 45
-     *
-     * Note, apps id may appear twice if they are in multiple cells.
-     * Note, all app relevant app ids are returned, this does not take into account the vantage point
-     *
-     * @param diagramId  diagram identifier
-     * @param aggregatedEntityKind
-     * @return a select statement returning a list of `{cellExtId, appId}` entries
-     */
-    protected static Select<Record2<String, Long>> mkOverlayEntityCellAggregateEntitySelector(DSLContext dsl,
-                                                                                              long diagramId,
-                                                                                              EntityKind aggregatedEntityKind) {
-
-        GenericSelectorFactory genericSelectorFactory = new GenericSelectorFactory();
-
-        SelectConditionStep<Record3<String, String, Long>> record3s = selectCellMappingsForDiagram(dsl, diagramId);
-
-        Set<Tuple3<String, String, Long>> fetch = dsl
-                .selectQuery(record3s)
-                .fetchSet(r -> tuple(r.get(0, String.class), r.get(1, String.class), r.get(2, Long.class)))
-                .stream()
-                .filter(t -> t.v1 == null || t.v2 == null || t.v3 == null)
-                .collect(toSet());
-
-        Set<Select<Record2<String, Long>>> stuffToUnion = record3s
-                .fetchSet(r -> {
-                    String relatedKind = r.get(AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA.RELATED_ENTITY_KIND);
-                    Long relatedId = r.get("related_entity_id", Long.class);
-
-                    IdSelectionOptions idSelectionOptions = mkOpts(EntityReference.mkRef(EntityKind.valueOf(relatedKind), relatedId));
-
-                    GenericSelector entityIdSelector = genericSelectorFactory.applyForKind(aggregatedEntityKind, idSelectionOptions);
-
-                    return DSL
-                            .select(DSL.val(r.get(AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA.CELL_EXTERNAL_ID)).as("cell_ext_id"),
-                                    entityIdSelector.selector().field(0, Long.class))
-                            .from(entityIdSelector.selector());
-                });
-
-        return stuffToUnion
-                .stream()
-                .reduce(null,
-                        (acc, r) -> {
-                            if (acc == null) {
-                                return r;
-                            } else {
-                                return acc.union(r);
-                            }
-                        });
-    }
-
-
-    protected static SelectConditionStep<Record3<String, String, Long>> selectCellMappingsForDiagram(DSLContext dsl,
-                                                                                                     long diagramId) {
+    protected static Set<Tuple2<String, EntityReference>> loadExpandedCellMappingsForDiagram(DSLContext dsl,
+                                                                                             long diagramId) {
 
         Field<Long> related_entity_id = DSL
                 .coalesce(ENTITY_HIERARCHY.ID, AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA.RELATED_ENTITY_ID)
@@ -134,32 +60,134 @@ public class AggregateOverlayDiagramUtilities {
                 .leftJoin(ENTITY_HIERARCHY)
                 .on(AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA.RELATED_ENTITY_ID.eq(ENTITY_HIERARCHY.ANCESTOR_ID))
                 .and(ENTITY_HIERARCHY.KIND.eq(AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA.RELATED_ENTITY_KIND))
+                .where(AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA.DIAGRAM_ID.eq(diagramId))
+                .fetchSet(r -> tuple(
+                        r.get(0, String.class), //cellExtId
+                        readRef(r, r.field2(), r.field3())));
+    }
+
+
+    protected static SelectConditionStep<Record3<String, String, Long>> selectCellMappingsForDiagram2(DSLContext dsl,
+                                                                                                      long diagramId) {
+
+        return dsl
+                .selectDistinct(
+                        AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA.CELL_EXTERNAL_ID,
+                        AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA.RELATED_ENTITY_KIND,
+                        AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA.RELATED_ENTITY_ID)
+                .from(AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA)
                 .where(AGGREGATE_OVERLAY_DIAGRAM_CELL_DATA.DIAGRAM_ID.eq(diagramId));
     }
 
 
-    /**
-     * Takes the maximal set of app ids that may appear on the diagram (derived by unioning the values of
-     * the cellExtIdsToEntityIdsMap) and filters then by the apps associated to the vantage point (given via
-     * the inScopeAggregatedEntitySelector).
-     * <p>
-     * Returns only appIds which should appear on the diagram  (basically the intersection between the
-     * maximal set given diagram cell mappings and the vantage point selector).
-     */
-    protected static Set<Long> calcExactEntityIdsOnDiagram(DSLContext dsl,
-                                                           Map<String, Set<Long>> cellExtIdsToEntityIdsMap,
-                                                           Select<Record1<Long>> inScopeAggregatedEntitySelector) {
-        Set<Long> maximalEntityIdsForCells = cellExtIdsToEntityIdsMap
-                .values()
+    protected static Map<String, Set<Long>> loadCellExtIdToAggregatedEntities(DSLContext dsl,
+                                                                              long diagramId,
+                                                                              EntityKind aggregatedEntityKind,
+                                                                              Select<Record1<Long>> inScopeEntityIdSelector) {
+
+        Set<Tuple2<String, EntityReference>> cellMappings = loadExpandedCellMappingsForDiagram(dsl, diagramId);
+
+        Map<String, Collection<EntityReference>> cellBackingEntitiesByCellExtId = groupBy(
+                cellMappings,
+                t -> t.v1,
+                t -> t.v2);
+
+        Set<Long> backingMeasurableEntityIds = cellMappings
                 .stream()
-                .flatMap(Collection::stream)
+                .map(Tuple2::v2)
+                .filter(d -> d.kind() == EntityKind.MEASURABLE)
+                .map(EntityReference::id)
                 .collect(toSet());
 
-        return dsl
-                .select(inScopeAggregatedEntitySelector.field(0, Long.class))
-                .from(inScopeAggregatedEntitySelector)
-                .where(inScopeAggregatedEntitySelector.field(0).in(maximalEntityIdsForCells))
-                .fetchSet(0, Long.class);
+        Map<Long, List<Long>> measurableIdToEntityIds = findMeasurableIdToAggregatedEntityIdMap(
+                dsl,
+                aggregatedEntityKind,
+                inScopeEntityIdSelector,
+                backingMeasurableEntityIds);
+
+        return cellBackingEntitiesByCellExtId
+                .entrySet()
+                .stream()
+                .map(e -> {
+                    Set<EntityReference> value = fromCollection(e.getValue());
+                    String cellExtId = e.getKey();
+
+                    Map<EntityKind, Set<Long>> aggregatedEntitiesByKind = value
+                            .stream()
+                            .collect(groupingBy(
+                                    EntityReference::kind,
+                                    mapping(EntityReference::id, toSet())));
+
+                    //only supports measurables at the moment
+                    Set<Long> measurableIds = aggregatedEntitiesByKind.getOrDefault(EntityKind.MEASURABLE, emptySet());
+
+                    Set<Long> entityIds = measurableIds
+                            .stream()
+                            .flatMap(mID -> measurableIdToEntityIds.getOrDefault(mID, emptyList()).stream())
+                            .collect(toSet());
+
+                    return tuple(cellExtId, entityIds);
+                })
+                .collect(toMap(k -> k.v1, k -> k.v2));
     }
 
+
+    private static Map<Long, List<Long>> findMeasurableIdToAggregatedEntityIdMap(DSLContext dsl,
+                                                                                 EntityKind aggregatedEntityKind,
+                                                                                 Select<Record1<Long>> inScopeEntityIdSelector,
+                                                                                 Set<Long> backingEntityIds) {
+
+        switch (aggregatedEntityKind) {
+            case APPLICATION:
+                return loadMeasurableToAppIdsMap(dsl, inScopeEntityIdSelector, backingEntityIds);
+            case CHANGE_INITIATIVE:
+                return loadMeasurableToCIIdsMap(dsl, inScopeEntityIdSelector, backingEntityIds);
+            default:
+                throw new IllegalArgumentException(format("Cannot load measurable id to entity map for entity kind: %s", aggregatedEntityKind));
+        }
+    }
+
+
+    private static Map<Long, List<Long>> loadMeasurableToCIIdsMap(DSLContext dsl,
+                                                                  Select<Record1<Long>> inScopeEntityIdSelector,
+                                                                  Set<Long> backingEntityIds) {
+
+        SelectConditionStep<Record2<Long, Long>> aToB = dsl
+                .selectDistinct(ENTITY_RELATIONSHIP.ID_B.as("measurable_id"), ENTITY_HIERARCHY.ID.as("ci_id"))
+                .from(ENTITY_RELATIONSHIP)
+                .innerJoin(ENTITY_HIERARCHY).on(ENTITY_RELATIONSHIP.ID_A.eq(ENTITY_HIERARCHY.ANCESTOR_ID)
+                        .and(ENTITY_HIERARCHY.KIND.eq(EntityKind.CHANGE_INITIATIVE.name())))
+                .where(ENTITY_RELATIONSHIP.KIND_A.eq(EntityKind.CHANGE_INITIATIVE.name()))
+                .and(ENTITY_RELATIONSHIP.KIND_B.eq(EntityKind.MEASURABLE.name())
+                        .and(ENTITY_HIERARCHY.ID.in(inScopeEntityIdSelector)
+                                .and(ENTITY_RELATIONSHIP.ID_B.in(backingEntityIds))));
+
+        SelectConditionStep<Record2<Long, Long>> bToA = dsl
+                .selectDistinct(ENTITY_RELATIONSHIP.ID_A.as("measurable_id"), ENTITY_RELATIONSHIP.ID_B.as("ci_id"))
+                .from(ENTITY_RELATIONSHIP)
+                .innerJoin(ENTITY_HIERARCHY).on(ENTITY_RELATIONSHIP.ID_A.eq(ENTITY_HIERARCHY.ANCESTOR_ID)
+                        .and(ENTITY_HIERARCHY.KIND.eq(EntityKind.CHANGE_INITIATIVE.name())))
+                .where(ENTITY_RELATIONSHIP.KIND_B.eq(EntityKind.CHANGE_INITIATIVE.name()))
+                .and(ENTITY_RELATIONSHIP.KIND_A.eq(EntityKind.MEASURABLE.name())
+                        .and(ENTITY_HIERARCHY.ID.in(inScopeEntityIdSelector)
+                                .and(ENTITY_RELATIONSHIP.ID_B.in(backingEntityIds))));
+
+        return aToB.union(bToA)
+                .fetchGroups(
+                        r -> r.get("measurable_id", Long.class),
+                        r -> r.get("ci_id", Long.class));
+    }
+
+
+    private static Map<Long, List<Long>> loadMeasurableToAppIdsMap(DSLContext dsl,
+                                                                   Select<Record1<Long>> inScopeEntityIdSelector,
+                                                                   Set<Long> backingEntityReferences) {
+        return dsl
+                .selectDistinct(MEASURABLE_RATING.MEASURABLE_ID, MEASURABLE_RATING.ENTITY_ID)
+                .from(MEASURABLE_RATING)
+                .where(MEASURABLE_RATING.ENTITY_KIND.eq(EntityKind.APPLICATION.name())
+                        .and(MEASURABLE_RATING.ENTITY_ID.in(inScopeEntityIdSelector))
+                        .and(MEASURABLE_RATING.MEASURABLE_ID.in(backingEntityReferences)))
+                .fetchGroups(MEASURABLE_RATING.MEASURABLE_ID, MEASURABLE_RATING.ENTITY_ID);
+    }
 }
--- waltz-data/src/main/java/org/finos/waltz/data/aggregate_overlay_diagram/AggregatedEntitiesWidgetDao.java
+++ waltz-data/src/main/java/org/finos/waltz/data/aggregate_overlay_diagram/AggregatedEntitiesWidgetDao.java
@@ -6,25 +6,16 @@ import org.finos.waltz.model.aggregate_overlay_diagram.overlay.AggregatedEntitie
 import org.finos.waltz.model.aggregate_overlay_diagram.overlay.ImmutableAggregatedEntitiesWidgetDatum;
 import org.finos.waltz.schema.Tables;
 import org.jooq.*;
-import org.jooq.lambda.tuple.Tuple2;
-import org.jooq.lambda.tuple.Tuple3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import static java.lang.String.format;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptySet;
-import static java.util.stream.Collectors.*;
-import static org.finos.waltz.data.aggregate_overlay_diagram.AggregateOverlayDiagramUtilities.selectCellMappingsForDiagram;
+import static java.util.stream.Collectors.toSet;
+import static org.finos.waltz.data.aggregate_overlay_diagram.AggregateOverlayDiagramUtilities.loadCellExtIdToAggregatedEntities;
 import static org.finos.waltz.model.EntityReference.mkRef;
-import static org.finos.waltz.schema.Tables.ENTITY_HIERARCHY;
-import static org.finos.waltz.schema.Tables.MEASURABLE_RATING;
-import static org.finos.waltz.schema.tables.EntityRelationship.ENTITY_RELATIONSHIP;
-import static org.jooq.lambda.tuple.Tuple.tuple;
 
 @Repository
 public class AggregatedEntitiesWidgetDao {
@@ -42,11 +33,15 @@ public class AggregatedEntitiesWidgetDao {
                                                              EntityKind aggregatedEntityKind,
                                                              Select<Record1<Long>> inScopeEntityIdSelector) {
 
-        Map<String, Set<Long>> cellExtIdsToEntityIdsMap = loadCellExtIdToEntityMap(dsl, diagramId, aggregatedEntityKind, inScopeEntityIdSelector);
+        Map<String, Set<Long>> cellExtIdsToAggregatedEntities = loadCellExtIdToAggregatedEntities(
+                dsl,
+                diagramId,
+                aggregatedEntityKind,
+                inScopeEntityIdSelector);
 
         Map<Long, String> entityIdToNameMap = loadEntityIdToNameMap(aggregatedEntityKind, inScopeEntityIdSelector);
 
-        return cellExtIdsToEntityIdsMap
+        return cellExtIdsToAggregatedEntities
                 .entrySet()
                 .stream()
                 .map(e -> {
@@ -94,93 +89,5 @@ public class AggregatedEntitiesWidgetDao {
     }
 
 
-    protected static Map<String, Set<Long>> loadCellExtIdToEntityMap(DSLContext dsl,
-                                                                     long diagramId,
-                                                                     EntityKind aggregatedEntityKind,
-                                                                     Select<Record1<Long>> inScopeEntityIdSelector) {
-
-        Map<Long, List<Long>> measurableIdToEntityIds = findMeasurableIdToEntityIdMap(dsl, aggregatedEntityKind, inScopeEntityIdSelector);
-
-        Set<Tuple3<String, String, Long>> record3s = selectCellMappingsForDiagram(dsl, diagramId)
-                .fetchSet(r -> tuple(r.get(0, String.class), r.get(1, String.class), r.get(2, Long.class)));
-
-        Map<String, Set<Tuple2<String, Long>>> relatedEntitiesByCellExtId = record3s
-                .stream()
-                .collect(groupingBy(t -> t.v1, mapping(t -> t.split1().v2, toSet())));
-
-        return relatedEntitiesByCellExtId
-                .entrySet()
-                .stream()
-                .map(e -> {
-                    Set<Tuple2<String, Long>> value = e.getValue();
-                    String cellExtId = e.getKey();
-
-                    Map<String, Set<Long>> aggregatedEntitiesByKind = value
-                            .stream()
-                            .collect(groupingBy(t -> t.v1, mapping(t -> t.v2, toSet())));
-
-                    Set<Long> measurableIds = aggregatedEntitiesByKind.getOrDefault(EntityKind.MEASURABLE.name(), emptySet());
-
-                    Set<Long> entityIds = measurableIds
-                            .stream()
-                            .flatMap(mID -> measurableIdToEntityIds.getOrDefault(mID, emptyList()).stream())
-                            .collect(toSet());
-
-                    return tuple(cellExtId, entityIds);
-                })
-                .collect(toMap(k -> k.v1, k -> k.v2));
-    }
-
-
-    private static Map<Long, List<Long>> findMeasurableIdToEntityIdMap(DSLContext dsl,
-                                                                       EntityKind aggregatedEntityKind,
-                                                                       Select<Record1<Long>> inScopeEntityIdSelector) {
-        switch (aggregatedEntityKind) {
-            case APPLICATION:
-                return loadMeasurableToAppIdsMap(dsl, inScopeEntityIdSelector);
-            case CHANGE_INITIATIVE:
-                return loadMeasurableToCIIdsMap(dsl, inScopeEntityIdSelector);
-            default:
-                throw new IllegalArgumentException(format("Cannot load measurable id to entity map for entity kind: %s", aggregatedEntityKind));
-        }
-    }
-
-
-    private static Map<Long, List<Long>> loadMeasurableToCIIdsMap(DSLContext dsl,
-                                                                  Select<Record1<Long>> inScopeEntityIdSelector) {
-
-        SelectConditionStep<Record2<Long, Long>> aToB = dsl
-                .selectDistinct(ENTITY_RELATIONSHIP.ID_B.as("measurable_id"), ENTITY_HIERARCHY.ID.as("ci_id"))
-                .from(ENTITY_RELATIONSHIP)
-                .innerJoin(ENTITY_HIERARCHY).on(ENTITY_RELATIONSHIP.ID_A.eq(ENTITY_HIERARCHY.ANCESTOR_ID)
-                        .and(ENTITY_HIERARCHY.KIND.eq(EntityKind.CHANGE_INITIATIVE.name())))
-                .where(ENTITY_RELATIONSHIP.KIND_A.eq(EntityKind.CHANGE_INITIATIVE.name()))
-                .and(ENTITY_RELATIONSHIP.KIND_B.eq(EntityKind.MEASURABLE.name())
-                        .and(ENTITY_HIERARCHY.ID.in(inScopeEntityIdSelector)));
-
-        SelectConditionStep<Record2<Long, Long>> bToA = dsl
-                .selectDistinct(ENTITY_RELATIONSHIP.ID_A.as("measurable_id"), ENTITY_RELATIONSHIP.ID_B.as("ci_id"))
-                .from(ENTITY_RELATIONSHIP)
-                .innerJoin(ENTITY_HIERARCHY).on(ENTITY_RELATIONSHIP.ID_A.eq(ENTITY_HIERARCHY.ANCESTOR_ID)
-                        .and(ENTITY_HIERARCHY.KIND.eq(EntityKind.CHANGE_INITIATIVE.name())))
-                .where(ENTITY_RELATIONSHIP.KIND_B.eq(EntityKind.CHANGE_INITIATIVE.name()))
-                .and(ENTITY_RELATIONSHIP.KIND_A.eq(EntityKind.MEASURABLE.name()))
-                .and(ENTITY_HIERARCHY.ID.in(inScopeEntityIdSelector));
-
-        return aToB.union(bToA)
-                .fetchGroups(r -> r.get("measurable_id", Long.class), r -> r.get("ci_id", Long.class));
-    }
-
-
-    private static Map<Long, List<Long>> loadMeasurableToAppIdsMap(DSLContext dsl, Select<Record1<Long>> inScopeEntityIdSelector) {
-        return dsl
-                .selectDistinct(ENTITY_HIERARCHY.ANCESTOR_ID, MEASURABLE_RATING.ENTITY_ID)
-                .from(ENTITY_HIERARCHY)
-                .leftJoin(MEASURABLE_RATING).on(MEASURABLE_RATING.MEASURABLE_ID.eq(ENTITY_HIERARCHY.ID)
-                        .and(ENTITY_HIERARCHY.KIND.eq(EntityKind.MEASURABLE.name())))
-                .where(MEASURABLE_RATING.ENTITY_KIND.eq(EntityKind.APPLICATION.name())
-                        .and(MEASURABLE_RATING.ENTITY_ID.in(inScopeEntityIdSelector)))
-                .fetchGroups(ENTITY_HIERARCHY.ANCESTOR_ID, MEASURABLE_RATING.ENTITY_ID);
-    }
 
 }
