f76aa59be150cc7477e33729c05b2ac844bf8d7b
David Watkins <davidwatkins73@gmail.com>
2022-09-05 12:39:07 +0100

    Survey lists show template name
    - was confusing if run's were given ambiguous names...
    
    #6210

--- waltz-ng/client/survey/components/svelte/SurveyApproverInfoPanel.svelte
+++ waltz-ng/client/survey/components/svelte/SurveyApproverInfoPanel.svelte
@@ -171,7 +171,10 @@
     <table class="table table-condensed">
         <thead>
         <tr>
-            <th width="30%">Survey Name</th>
+            <th width="30%"
+                style="text-align: left">
+                Survey Name
+            </th>
             {#each tableHeaders as header}
                 <th width={`${70 / tableHeaders.length}%`}
                     class={header.headerClass}
