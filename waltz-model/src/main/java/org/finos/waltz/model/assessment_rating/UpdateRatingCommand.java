263c31237d7050de2b2b32e43577f74b51f6173e
David Watkins <davidwatkins73@gmail.com>
2023-02-07 10:25:03 +0000

    Update UpdateRatingCommand.java

--- waltz-model/src/main/java/org/finos/waltz/model/assessment_rating/UpdateRatingCommand.java
+++ waltz-model/src/main/java/org/finos/waltz/model/assessment_rating/UpdateRatingCommand.java
@@ -3,11 +3,12 @@ package org.finos.waltz.model.assessment_rating;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
+import org.finos.waltz.model.command.Command;
 
 @Value.Immutable
 @JsonSerialize(as = ImmutableUpdateRatingCommand.class)
 @JsonDeserialize(as = ImmutableUpdateRatingCommand.class)
-public abstract class UpdateRatingCommand {
+public abstract class UpdateRatingCommand implement Command {
     public abstract Long newRatingId();
 
 }
