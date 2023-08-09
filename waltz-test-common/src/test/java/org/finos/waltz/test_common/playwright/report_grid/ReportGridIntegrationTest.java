8c402b731c67af02f17f7db12b75d39fc62065bc
David Watkins <davidwatkins73@gmail.com>
2023-08-09 21:22:01 +0100

    Report Grid UI tests
    #6071

--- waltz-test-common/src/test/java/org/finos/waltz/test_common/playwright/report_grid/ReportGridIntegrationTest.java
+++ waltz-test-common/src/test/java/org/finos/waltz/test_common/playwright/report_grid/ReportGridIntegrationTest.java
@@ -2,30 +2,27 @@ package org.finos.waltz.test_common.playwright.report_grid;
 
 
 import com.microsoft.playwright.Locator;
-import org.finos.waltz.common.SetUtilities;
 import org.finos.waltz.common.exception.InsufficientPrivelegeException;
+import org.finos.waltz.model.EntityKind;
 import org.finos.waltz.model.EntityReference;
-import org.finos.waltz.model.survey.ImmutableSurveyQuestion;
-import org.finos.waltz.model.survey.SurveyQuestion;
-import org.finos.waltz.model.survey.SurveyQuestionFieldType;
+import org.finos.waltz.model.assessment_definition.AssessmentVisibility;
 import org.finos.waltz.test_common.helpers.AppGroupHelper;
 import org.finos.waltz.test_common.helpers.AppHelper;
-import org.finos.waltz.test_common.helpers.InvolvementHelper;
-import org.finos.waltz.test_common.helpers.PersonHelper;
+import org.finos.waltz.test_common.helpers.AssessmentHelper;
+import org.finos.waltz.test_common.helpers.RatingSchemeHelper;
 import org.finos.waltz.test_common.playwright.BasePlaywrightIntegrationTest;
 import org.finos.waltz.test_common.playwright.ScreenshotHelper;
-import org.finos.waltz.test_common.playwright.Section;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
-import java.time.LocalDate;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static java.lang.String.format;
 import static org.finos.waltz.common.SetUtilities.asSet;
 import static org.finos.waltz.common.StringUtilities.mkPath;
+import static org.finos.waltz.model.EntityReference.mkRef;
 import static org.finos.waltz.test_common.helpers.NameHelper.mkName;
 import static org.finos.waltz.test_common.playwright.PlaywrightUtilities.*;
 
@@ -39,6 +36,11 @@ public class ReportGridIntegrationTest extends BasePlaywrightIntegrationTest {
     @Autowired
     private AppGroupHelper appGroupHelper;
 
+    @Autowired
+    private AssessmentHelper assessmentHelper;
+
+    @Autowired
+    private RatingSchemeHelper schemeHelper;
 
     @BeforeEach
     public void setup() throws IOException {
@@ -53,8 +55,11 @@ public class ReportGridIntegrationTest extends BasePlaywrightIntegrationTest {
                 page,
                 "screenshots/report-grid/create-grid");
 
-        EntityReference appRef = appHelper.createNewApp(name, 10L);
-        Long gId = appGroupHelper.createAppGroupWithAppRefs(name, asSet(appRef));
+        EntityReference appRef1 = appHelper.createNewApp(mkName("rg-app1"), 10L);
+        EntityReference appRef2 = appHelper.createNewApp(mkName("rg-app2"), 10L);
+        EntityReference assessmentRef = setupAssessments(appRef1, appRef2);
+
+        Long gId = appGroupHelper.createAppGroupWithAppRefs(name, asSet(appRef1, appRef2));
 
         page.navigate(mkPath(BASE, "app-group", Long.toString(gId)));
         screenshotHelper.takePageSnapshot(page, "new_app_group.png");
@@ -82,13 +87,27 @@ public class ReportGridIntegrationTest extends BasePlaywrightIntegrationTest {
         addColumn(screenshotHelper, "Cost Kind", "Application Development");
         addColumn(screenshotHelper, "Attestation ", "Logical Flow");
         addColumn(screenshotHelper, "Involvement Kind ", "IT Architect");
+        addColumn(screenshotHelper, "Assessment Definition", assessmentRef.name().get());
 
         page.locator(".btn-success").getByText("Save this Report").click();
         screenshotHelper.takePageSnapshot(page, "saved_report.png");
 
         Locator grid = page.locator("waltz-grid-with-search");
         screenshotHelper.takeElemSnapshot(grid, "grid_data.png");
-        assertThat(grid.locator("div.ui-grid-cell").getByTestId("entity-name").getByText(name)).isVisible();
+        assertThat(grid.locator("div.ui-grid-cell").getByTestId("entity-name").getByText(appRef1.name().get())).isVisible();
+        assertThat(grid.locator("div.ui-grid-cell").getByTestId("entity-name").getByText(appRef2.name().get())).isVisible();
+    }
+
+
+    private EntityReference setupAssessments(EntityReference appRef1, EntityReference appRef2) {
+        String name = mkName("rg-assessment");
+        long schemeId = schemeHelper.createEmptyRatingScheme(name);
+        Long redId = schemeHelper.saveRatingItem(schemeId, "red", 0, "red", "R");
+        Long greenId = schemeHelper.saveRatingItem(schemeId, "green", 0, "green", "G");
+        long defId = assessmentHelper.createDefinition(schemeId, name, null, AssessmentVisibility.SECONDARY, "test");
+        assessmentHelper.createAssessment(defId, appRef1, redId);
+        assessmentHelper.createAssessment(defId, appRef2, greenId);
+        return mkRef(EntityKind.ASSESSMENT_DEFINITION, defId, name);
     }
 
 
