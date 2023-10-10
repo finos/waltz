87117238e1576ceb7b5361f09908c5ac715f3fc1
David Watkins <davidwatkins73@gmail.com>
2023-10-10 13:38:16 +0100

    Adding logical flow decorator support to assessments
    #6798

--- waltz-ng/client/common/services/enums/entity.js
+++ waltz-ng/client/common/services/enums/entity.js
@@ -269,6 +269,13 @@ export const entity = {
         description: null,
         position: 1600
     },
+    LOGICAL_DATA_FLOW_DATA_TYPE_DECORATOR: {
+        key: "LOGICAL_DATA_FLOW_DATA_TYPE_DECORATOR",
+        name: "Logical Flow Data Type",
+        icon: "barcode",
+        description: null,
+        position: 1650
+    },
     ORG_UNIT: {
         key: "ORG_UNIT",
         name: "Org Unit",
@@ -324,6 +331,13 @@ export const entity = {
         icon: "file-text",
         description: null,
         position: 2100
+    },
+    PHYSICAL_SPEC_DATA_TYPE_DECORATOR: {
+        key: "PHYSICAL_SPEC_DATA_TYPE_DECORATOR",
+        name: "Physical Specification Data Type",
+        icon: "barcode",
+        description: null,
+        position: 2150
     },
     PHYSICAL_FLOW: {
         key: "PHYSICAL_FLOW",
