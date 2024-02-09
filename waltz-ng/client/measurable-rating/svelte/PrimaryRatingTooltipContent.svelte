7ab92eeb726461215e5a818b6be15ecaa008d925
woodjes <jessica.woodland-scott@db.com>
2024-02-09 14:34:06 +0000

    Add primary ratings to app overview
    #CTCTOWALTZ-3001
    #6820

---
+++ waltz-ng/client/measurable-rating/svelte/PrimaryRatingTooltipContent.svelte
@@ -0,0 +1,40 @@
+<script>
+
+    import DescriptionFade from "../../common/svelte/DescriptionFade.svelte";
+    import RatingIndicatorCell from "../../ratings/components/rating-indicator-cell/RatingIndicatorCell.svelte";
+
+    export let rating;
+    export let ratingSchemeItem;
+    export let measurable;
+
+</script>
+
+
+{#if measurable}
+    <div>
+        <h4>{measurable.name}</h4>
+        <span class="text-muted">{measurable.externalId}</span>
+        <DescriptionFade text={measurable.description}/>
+    </div>
+    <hr>
+{/if}
+
+<div>
+    <RatingIndicatorCell {...ratingSchemeItem}
+                         showName="true"/>
+    <div>
+        {ratingSchemeItem.description}
+    </div>
+</div>
+
+{#if rating.description}
+    <hr>
+    <div>
+        <div>
+            Rating Comment:
+        </div>
+        <DescriptionFade text={rating.description || ""}/>
+    </div>
+{:else }
+    <br>
+{/if}
