d831a258731b23238c6854862a818998bf87c6d6
David Watkins <davidwatkins73@gmail.com>
2024-03-23 21:22:20 +0000

    Licences - search for licences from navbar
    #7039

--- waltz-common/src/test/java/org/finos/waltz/common/ListUtilities_distinct.java
+++ waltz-common/src/test/java/org/finos/waltz/common/ListUtilities_distinct.java
@@ -1,12 +1,10 @@
 package org.finos.waltz.common;
 
-import org.jooq.lambda.tuple.Tuple2;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.finos.waltz.common.ListUtilities.asList;
-import static org.jooq.lambda.tuple.Tuple.tuple;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ListUtilities_distinct {
