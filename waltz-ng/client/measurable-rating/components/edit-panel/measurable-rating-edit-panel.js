156b3e3aec6ce90ec2f2871f353b5923a46219f8
Kuldeep-A Jindal <kuldeep-a.jindal@db.com>
2025-02-20 14:51:21 +0530

    added static panel for notification

--- waltz-ng/client/measurable-rating/components/edit-panel/measurable-rating-edit-panel.js
+++ waltz-ng/client/measurable-rating/components/edit-panel/measurable-rating-edit-panel.js
@@ -348,12 +348,6 @@ function controller($q,
                 })
                 .finally(() => {
                     deselectMeasurable();
-                    if(vm.selected.category.category.allowPrimaryRatings){
-                        alert('Primary Vs Non-Primary flag tagging for Application to measurable mapping\n' +
-                            'a. Click again on Level 3 to view the selection button ‘Primary’.\n' +
-                            'b. If it is a key measurable for the application, click on ‘Primary’ button.\n' +
-                            'c. Note: Atleast 1 measurable should be mapped as ‘Primary’ for the application.');
-                    }
                 });
     };
 
