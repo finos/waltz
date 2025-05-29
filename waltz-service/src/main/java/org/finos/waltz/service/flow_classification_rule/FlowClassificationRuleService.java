9602f032a92c22c09accd82d7e5958af8f0e422a
jainshrk <shreyans.jain@db.com>
2025-05-29 10:16:40 +0530

    filter out empty strings
    from creating an entry in the flow classification rules priorities map

--- waltz-service/src/main/java/org/finos/waltz/service/flow_classification_rule/FlowClassificationRuleService.java
+++ waltz-service/src/main/java/org/finos/waltz/service/flow_classification_rule/FlowClassificationRuleService.java
@@ -293,6 +293,7 @@ public class FlowClassificationRuleService {
                     .get()
                     .split(","))
                     .map(String::trim)
+                    .filter(String::isEmpty)
                     .collect(Collectors.toList());
 
             IntStream.range(0, prioritiesList.size())
