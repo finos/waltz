f33153a7b60ca036d722e9380934ed6b2a987821
jainshrk <shreyans.jain@db.com>
2025-07-30 10:38:27 +0530

    propose data flow view - setup

--- waltz-ng/client/data-flow/components/svelte/propose-data-flow/svelte/ProposeDataFlowView.svelte
+++ waltz-ng/client/data-flow/components/svelte/propose-data-flow/svelte/ProposeDataFlowView.svelte
@@ -1,9 +1,75 @@
 <script>
+    import _ from "lodash";
+    import PageHeader from "../../../../../common/svelte/PageHeader.svelte";
+    import ViewLink from "../../../../../common/svelte/ViewLink.svelte";
+    import EntityLink from "../../../../../common/svelte/EntityLink.svelte";
+    import { loadSvelteEntity } from "../../../../../common/entity-utils";
+    import NoData from "../../../../../common/svelte/NoData.svelte";
+    import ApplicationInfoPanel from "../../../../../common/svelte/info-panels/ApplicationInfoPanel.svelte";
+    import RouteSelector from "../../../../../physical-flows/svelte/RouteSelector.svelte";
+    import LogicalFlowSelectionStep from "../../../../../physical-flows/svelte/LogicalFlowSelectionStep.svelte";
+    import PhysicalFlowCharacteristicsStep
+        from "../../../../../physical-flows/svelte/PhysicalFlowCharacteristicsStep.svelte";
+    import PhysicalSpecificationStep from "../../../../../physical-flows/svelte/PhysicalSpecificationStep.svelte";
+    import DataTypeSelectionStep from "../../../../../physical-flows/svelte/DataTypeSelectionStep.svelte";
+    import Icon from "../../../../../common/svelte/Icon.svelte";
     export let parentEntityRef;
+
+    $: sourceEntityCall = loadSvelteEntity(parentEntityRef);
+    $: sourceEntity = $sourceEntityCall.data ?
+        $sourceEntityCall.data
+        : {};
+
 </script>
-<div>
-    {#if parentEntityRef}
-        {parentEntityRef.id}: {parentEntityRef.kind}
-    {/if}
-    Hello, Data Flow
-</div>
+
+
+<PageHeader name="Propose Data Flow"
+            icon="code-pull-request"
+            small={_.get(sourceEntity, ["name"], "-")}>
+    <div slot="breadcrumbs">
+        <ol class="waltz-breadcrumbs">
+            <li><ViewLink state="main">Home</ViewLink></li>
+            <li><EntityLink ref={sourceEntity}/></li>
+            <li>Propose Data Flow</li>
+        </ol>
+    </div>
+    <div slot="summary">
+        {#if !sourceEntity.name}
+            <NoData>
+                No data found for {parentEntityRef.kind} {parentEntityRef.id}
+            </NoData>
+        {:else}
+            <div class="selection-step">
+                <LogicalFlowSelectionStep primaryEntityRef={parentEntityRef}/>
+            </div>
+
+            <div class="selection-step">
+                <PhysicalSpecificationStep primaryEntityRef={parentEntityRef}/>
+            </div>
+
+            <div class="selection-step">
+                <PhysicalFlowCharacteristicsStep primaryEntityRef={parentEntityRef}/>
+            </div>
+
+            <div class="selection-step">
+                <DataTypeSelectionStep primaryEntityRef={parentEntityRef}/>
+            </div>
+            <br>
+
+            <span>
+                <button class="btn btn-success"
+                        disabled={true}
+                        on:click={() => true}>
+                    Create
+                </button>
+
+                {#if true}
+                    <span class="incomplete-warning">
+                        <Icon name="exclamation-triangle"/>You must complete all sections
+                    </span>
+                {/if}
+            </span>
+
+        {/if}
+    </div>
+</PageHeader>
