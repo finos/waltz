c4d0328640abd1b9c8f8a8dc7aed0a0eef0c32d4
jainkhu <khushi-d.jain@db.com>
2026-03-04 11:24:11 +0530

    removed extra spaces

--- waltz-service/src/main/java/org/finos/waltz/service/data_flow/DataFlowService.java
+++ waltz-service/src/main/java/org/finos/waltz/service/data_flow/DataFlowService.java
@@ -75,7 +75,7 @@ public class DataFlowService {
      * Creates logical and physical flows from the ProposedFlow.
      *
      * @param proposedFlowId primary key of the ProposedFlow
-     * @param username       actor requesting the creation
+     * @param username actor requesting the creation
      * @return immutable response containing the created flows
      * @throws FlowCreationException if either creation step fails
      */
@@ -211,7 +211,7 @@ public class DataFlowService {
      * associated logical flow together with its data-type decorators.
      *
      * @param proposedFlow as input
-     * @param username     user performing the operation
+     * @param username user performing the operation
      * @return immutable response object with the ids of the deleted artefacts
      */
     public DeletePhysicalFlowResponse deletePhysicalFlow(ProposedFlowResponse proposedFlow, String username) {
