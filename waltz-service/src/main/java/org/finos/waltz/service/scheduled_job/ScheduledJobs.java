64b00b7de49de86c7e93330db9880d3502ef663e
jainkhu <khushi-d.jain@db.com>
2026-03-25 18:25:33 +0530

    created a parallel scheduledJob service and added jobKey for the new job

---
+++ waltz-service/src/main/java/org/finos/waltz/service/scheduled_job/ScheduledJobs.java
@@ -0,0 +1,55 @@
+package org.finos.waltz.service.scheduled_job;
+
+import org.finos.waltz.common.ExcludeFromIntegrationTesting;
+import org.finos.waltz.data.scheduled_job.ScheduledJobDao;
+import org.finos.waltz.model.scheduled_job.JobKey;
+import org.finos.waltz.model.scheduled_job.JobLifecycleStatus;
+import org.finos.waltz.service.proposed_flow_workflow.ProposedFlowWorkflowService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.function.Consumer;
+
+@ExcludeFromIntegrationTesting
+@Service
+public class ScheduledJobs {
+    private static final Logger LOG = LoggerFactory.getLogger(ScheduledJobs.class);
+
+    private final ScheduledJobDao scheduledJobDao;
+    private final ProposedFlowWorkflowService proposedFlowWorkflowService;
+
+    @Autowired
+    public ScheduledJobs( ScheduledJobDao scheduledJobDao, ProposedFlowWorkflowService proposedFlowWorkflowService) {
+        this.scheduledJobDao = scheduledJobDao;
+        this.proposedFlowWorkflowService = proposedFlowWorkflowService;
+    }
+
+    @Scheduled(cron = "${waltz.proposed-flow.pending-timeout-cron:-}")
+    public void run() {
+        LOG.info("Running scheduled jobs");
+        runIfNeeded(JobKey.PENDING_FLOWS_TIME_OUT, proposedFlowWorkflowService::pendingFlowsTimeOut);
+    }
+    private void runIfNeeded(JobKey jobKey, Consumer<JobKey> jobExecutor) {
+        runIfNeeded(jobKey, jobExecutor, Collections.emptySet());
+    }
+
+    private void runIfNeeded(JobKey jobKey, Consumer<JobKey> jobExecutor, Set<JobKey> deadlockJobKeys) {
+        try {
+            if (scheduledJobDao.isJobRunnable(jobKey)
+                    && !scheduledJobDao.anyJobsRunning(deadlockJobKeys)
+                    && scheduledJobDao.markJobAsRunning(jobKey)) {
+                jobExecutor
+                        .andThen((jk) -> scheduledJobDao.updateJobStatus(jk, JobLifecycleStatus.COMPLETED))
+                        .accept(jobKey);
+            }
+        } catch (Exception e) {
+            LOG.error("Failed to run job: " + jobKey, e);
+            scheduledJobDao.updateJobStatus(jobKey, JobLifecycleStatus.ERRORED);
+        }
+    }
+}
